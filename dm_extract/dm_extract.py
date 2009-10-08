c502de267393ad5f77da25f893947a3e4666466d
ssi (Open ERP) <ssi@tinyerp.co.in>
2009-10-08 11:37:03 +0530

    [IMP] modified dm_extract

--- dm_extract/dm_extract.py
+++ dm_extract/dm_extract.py
@@ -19,62 +19,70 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import time
-
 from osv import fields
 from osv import osv
-import pooler
-import sys
-import datetime
-import netsvc
 
 class dm_address_segmentation(osv.osv): # {{{
     _name = "dm.address.segmentation"
     _description = "Segmentation"
 
     _columns = {
-        'name' : fields.char('Name', size=64, required=True),
-        'code' : fields.char('Code', size=32, required=True),
-        'notes' : fields.text('Description'),
-        'sql_query' : fields.text('SQL Query'),
-        'query_criteria_ids' : fields.one2many('dm.query.criteria', 'segmentation_id', 'Query Criteria'),
-#        'address_numeric_criteria_ids' : fields.one2many('dm.address.numeric_criteria', 'segmentation_id', 'Address Numeric Criteria'),
-#        'address_boolean_criteria_ids' : fields.one2many('dm.address.boolean_criteria', 'segmentation_id', 'Address Boolean Criteria'),
-#        'address_date_criteria_ids' : fields.one2many('dm.address.date_criteria', 'segmentation_id', 'Address Date Criteria'),
+        'name': fields.char('Name', size=64, required=True),
+        'code': fields.char('Code', size=32, required=True),
+        'notes': fields.text('Description'),
+        'sql_query': fields.text('SQL Query'),
+        'query_criteria_ids': fields.one2many('dm.query.criteria', 
+                                        'segmentation_id', 'Query Criteria'),
+#        'address_numeric_criteria_ids':
+#            fields.one2many('dm.address.numeric_criteria',
+#                                 'segmentation_id','Address Numeric Criteria'),
+#        'address_boolean_criteria_ids':
+#            fields.one2many('dm.address.boolean_criteria',
+#                                'segmentation_id','Address Boolean Criteria'),
+#        'address_date_criteria_ids': 
+#            fields.one2many('dm.address.date_criteria',
+#                                    'segmentation_id','Address Date Criteria'),
     }
 
-    def set_address_criteria(self, cr, uid, id, context={}):
-        query_obj = self.browse(cr, uid, id).query_criteria_ids
+    def set_address_criteria(self, cr, uid, ids, context={}):
+        query_obj = self.browse(cr, uid, ids).query_criteria_ids
         where_clause = []
-        for q_obj in query_obj :
-            value = getattr(q_obj,'value_'+q_obj.field_type)
+        for q_obj in query_obj:
+            value = getattr(q_obj, 'value_'+q_obj.field_type)
             if q_obj.field_type == 'boolean':
-                where_clause.append("rpa.%s %s %s"%(q_obj.field_id.name,q_obj.operator.code,value))
+                where_clause.append("rpa.%s %s %s"% (q_obj.field_id.name,
+                                    q_obj.operator.code,value))
             if q_obj.field_type == 'date':
-                where_clause.append("rpa.%s %s '%s'"%(q_obj.field_id.name,q_obj.operator.code,value))
+                where_clause.append("rpa.%s %s '%s'"% (q_obj.field_id.name,
+                                    q_obj.operator.code,value))
             if q_obj.field_type == 'char':
-                where_clause.append("rpa.%s %s '%s'"%(q_obj.field_id.name,q_obj.operator.code,'%'+value+'%'))
+                where_clause.append("rpa.%s %s '%s'"% (q_obj.field_id.name,
+                                    q_obj.operator.code,'%'+ value+ '%'))
             if q_obj.field_type == 'integer':
-                where_clause.append("rpa.%s %s %f"%(q_obj.field_id.name,q_obj.operator.code,value))
+                where_clause.append("rpa.%s %s %f"% (q_obj.field_id.name,
+                                    q_obj.operator.code, value))
         if where_clause:
-            sql_query = """select distinct rpa.id\n from res_partner_address \n where %s"""%(' and '.join(where_clause))
-        else :
-            sql_query = """select distinct rpa.name \nfrom res_partner_address rpa """
+            sql_query = """select distinct rpa.id\n from res_partner_address \n 
+                                where %s"""% (' and '.join(where_clause))
+        else:
+            sql_query = """select distinct rpa.name \n
+                            from res_partner_address rpa """
         return sql_query
 
-    def create(self,cr,uid,vals,context={}):
-        id = super(dm_address_segmentation,self).create(cr,uid,vals,context)
-        sql_query = self.set_address_criteria(cr, uid, id)
-        self.write(cr, uid, id, {'sql_query':sql_query})
-        return id
+    def create(self, cr, uid, vals, context={}):
+        ids =super(dm_address_segmentation, self).create(cr, uid, vals, context)
+        sql_query = self.set_address_criteria(cr, uid, ids)
+        self.write(cr, uid, ids, {'sql_query':sql_query})
+        return ids
 
     def write(self, cr, uid, ids, vals, context=None):
-        super(dm_address_segmentation,self).write(cr, uid, ids, vals, context)
+        super(dm_address_segmentation, self).write(cr, uid, ids, vals, context)
         if isinstance(ids, (int, long)):
             ids = [ids]
         for i in ids:
             sql_query = self.set_address_criteria(cr, uid, i)
-            super(dm_address_segmentation,self).write(cr, uid, i, {'sql_query':sql_query})
+            super(dm_address_segmentation, self).write(cr, uid, i, 
+                                                {'sql_query':sql_query})
         return ids
 
 dm_address_segmentation() # }}}
@@ -84,7 +92,8 @@ class dm_campaign_proposition_segment(osv.osv):
     _description = "Segmentation"
     _inherit = "dm.campaign.proposition.segment"
     _columns = {
-                'segmentation_id':fields.many2one('dm.address.segmentation','Segments'),
+                'segmentation_id':fields.many2one('dm.address.segmentation',
+                                                  'Segments'),
                 }
     
 dm_campaign_proposition_segment()
@@ -123,9 +132,9 @@ class dm_query_operator(osv.osv): # {{{
     _name = "dm.query.operator"
     _description = "Query Openrators"
     _columns = {
-        'field_type' : fields.selection(_OPERATORS, 'Field Type', size=32) ,
-        'name' : fields.char('Name', size=32),
-        'code' : fields.char('code', size=32),
+        'field_type': fields.selection(_OPERATORS, 'Field Type', size=32) ,
+        'name': fields.char('Name', size=32),
+        'code': fields.char('code', size=32),
     }
 dm_query_operator() # }}}
 
@@ -135,14 +144,16 @@ class dm_query_criteria(osv.osv): # {{{
     _rec_name = "segmentation_id"
 
     _columns = {
-        'segmentation_id' : fields.many2one('dm.address.segmentation', 'Segmentation'),
-        'field_id' : fields.many2one('ir.model.fields','Field'),
-        'field_type' : fields.selection(_OPERATORS, 'Field Type', size=32) ,
-        'operator' : fields.many2one('dm.query.operator','Operator'),
-        'value_char' : fields.char('Value', size=128),
-        'value_integer' : fields.float('Value', digits=(16,2)),
-        'value_boolean' : fields.selection([('true','True'),('false','False')],'Value'),
-        'value_date' : fields.date('Date'),
+        'segmentation_id':fields.many2one('dm.address.segmentation',
+                                          'Segmentation'),
+        'field_id':fields.many2one('ir.model.fields', 'Field'),
+        'field_type':fields.selection(_OPERATORS, 'Field Type', size=32) ,
+        'operator':fields.many2one('dm.query.operator', 'Operator'),
+        'value_char':fields.char('Value', size=128),
+        'value_integer':fields.float('Value', digits=(16,2)),
+        'value_boolean':fields.selection([('true','True'),('false','False')],
+                                         'Value'),
+        'value_date':fields.date('Date'),
     }
 dm_query_criteria() # }}}
 
@@ -153,17 +164,16 @@ class ir_model_fields(osv.osv):
             context=None, count=False):
         if context.has_key('name') and context['name']=='dm_fields':
             if context['ttype'] == 'char':
-                query_obj = self.pool.get('dm.query.criteria')
-                query_ids = query_obj.search(cr, uid, [])
-                for query_id in query_ids:
-                    cr.execute("select id from ir_model_fields where model='res.partner.address' and (ttype='many2one' or ttype = '%s')" %(context['ttype']))
-                    fields_ids = cr.fetchall()
-                    fields = map(lambda x: x[0], fields_ids)
-                    return fields
-                else:
-                    field_id = super(ir_model_fields, self).search(cr, uid, args, offset, limit,
-                                                    order, context=context, count=count)
-                    return field_id
+                cr.execute("select id from ir_model_fields where \
+                            model='res.partner.address' and \
+                        (ttype='many2one' or ttype = '%s')" %(context['ttype']))
+                fields_ids = cr.fetchall()
+                fields = map(lambda x: x[0], fields_ids)
+                return fields
+            else:
+                field_id = super(ir_model_fields, self).search(cr, uid, 
+                 args, offset, limit, order, context=context, count=count)
+                return field_id
 
 ir_model_fields()
 
@@ -173,13 +183,15 @@ ir_model_fields()
     _rec_name = "segmentation_id"
 
     _columns = {
-        'segmentation_id' : fields.many2one('dm.address.segmentation', 'Segmentation'),
-        'field_id' : fields.many2one('ir.model.fields','Address Field',
+        'segmentation_id':fields.many2one('dm.address.segmentation',
+                                            'Segmentation'),
+        'field_id': fields.many2one('ir.model.fields','Address Field',
                domain=[('model_id.model','=','res.partner.address'),
                ('ttype','in',['integer','float'])],
                context={'model':'res.partner.address'},required=True),
-        'operator' : fields.selection(NUMERIC_OPERATORS, 'Operator', size=32,required=True),
-        'value' : fields.float('Value', digits=(16,2),required=True),
+        'operator':fields.selection(NUMERIC_OPERATORS,'Operator',size=32,
+                                                    required=True),
+        'value': fields.float('Value', digits=(16,2),required=True),
     }
 dm_address_numeric_criteria() # }}}
 
@@ -189,13 +201,16 @@ class dm_address_boolean_criteria(osv.osv): # {{{
     _rec_name = "segmentation_id"
 
     _columns = {
-        'segmentation_id' : fields.many2one('dm.address.segmentation', 'Segmentation'),
-        'field_id' : fields.many2one('ir.model.fields','Address Field',
+        'segmentation_id':fields.many2one('dm.address.segmentation',
+                                            'Segmentation'),
+        'field_id':fields.many2one('ir.model.fields','Address Field',
                domain=[('model_id.model','=','res.partner.address'),
-               ('ttype','like','boolean')],
+                           ('ttype','like','boolean')],
                context={'model':'res.partner.address'},required=True),
-        'operator' : fields.selection(BOOL_OPERATORS, 'Operator', size=32,required=True),
-        'value' : fields.selection([('true','True'),('false','False')],'Value',required=True),
+        'operator':fields.selection(BOOL_OPERATORS,'Operator',size=32,
+                                        required=True),
+        'value':fields.selection([('true','True'),('false','False')],
+                                    'Value',required=True),
     }
 dm_address_boolean_criteria() # }}}
 
@@ -205,13 +220,15 @@ class dm_address_date_criteria(osv.osv): # {{{
     _rec_name = "segmentation_id"
 
     _columns = {
-        'segmentation_id' : fields.many2one('dm.address.segmentation', 'Segmentation'),
-        'field_id' : fields.many2one('ir.model.fields','Address Field',
+        'segmentation_id':fields.many2one('dm.address.segmentation',
+                                                'Segmentation'),
+        'field_id':fields.many2one('ir.model.fields','Address Field',
                domain=[('model_id.model','=','res.partner.address'),
                ('ttype','in',['date','datetime'])],
                context={'model':'res.partner.address'},required=True),
-        'operator' : fields.selection(DATE_OPERATORS, 'Operator', size=32,required=True),
-        'value' : fields.date('Date',required=True),
+        'operator':fields.selection(DATE_OPERATORS,'Operator',size=32,
+                                        required=True),
+        'value': fields.date('Date',required=True),
     }
 dm_address_date_criteria() # }}}'''
 

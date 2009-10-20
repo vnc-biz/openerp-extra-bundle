bcda26343a69919f37e85e2f95ba35c0395a0b1d
ssi (Open ERP) <ssi@tinyerp.co.in>
2009-10-20 14:42:13 +0530

    [IMP] quality improvement dm_simulator

--- dm_simulator/script/gen_partner.py
+++ dm_simulator/script/gen_partner.py
@@ -1,23 +1,25 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 import xmlrpclib
 import time
 
@@ -39,13 +41,16 @@ start_time = time.time()
 
 print "Creating %s partners" % no_partner
 
-for i in range(1,int(no_partner)+1):
-    p_id = server.execute(db,uid,pwd,'res.partner','create',{'name':str('P%%0%sd' % padding % i)})
-    a_id = server.execute(db,uid,pwd,'res.partner.address','create',{'name':str('Address of P%%0%sd' % padding % i),'partner_id':p_id})
+for i in range(1, int(no_partner)+1):
+    p_id= server.execute(db, uid, pwd, 'res.partner', 'create', 
+                         {'name': str('P%%0%sd' % padding % i)})
+    a_id= server.execute(db, uid, pwd, 'res.partner.address', 'create',
+                          {'name': str('Address of P%%0%sd' % padding % i), 
+                           'partner_id': p_id})
 #    print p_id, a_id
 
 end_time = time.time()
 delta = end_time - start_time
 
-print "%s partners created in %s seconds"% (no_partner,str(delta))
+print "%s partners created in %s seconds"% (no_partner, str(delta))
 

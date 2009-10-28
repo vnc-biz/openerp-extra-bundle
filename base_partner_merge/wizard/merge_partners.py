1a2409b9892912a9255b19bfb83f129c838ea0e2
Quentin De Paoli <qdp@cyan>
2009-10-28 10:35:55 +0100

    [FIX] fixed error on base_partner_merge module

--- base_partner_merge/wizard/merge_partners.py
+++ base_partner_merge/wizard/merge_partners.py
@@ -167,33 +167,41 @@ class wizard_merge_partners(wizard.interface):
             unique_fields = []
             for const in pool.get('res.partner')._sql_constraints:
                 c_names.append('res_partner_' + const[0])
-            c_names = tuple(map(lambda x: "'"+ x +"'", c_names))
-            cr.execute("""select column_name from \
+            if c_names:
+                c_names = tuple(map(lambda x: "'"+ x +"'", c_names))
+                print 'yo?', c_names
+                cr.execute("""select column_name from \
                         information_schema.constraint_column_usage u \
                         join  pg_constraint p on (p.conname=u.constraint_name) \
                         where u.constraint_name in (%s) and p.contype='u' """ % c_names)
-            for i in cr.fetchall():
-                remove_field[i[0]] = None
-                unique_fields.append(i[0])
-            unique_fields.append('name')
-            unique_data = pool.get('res.partner').read(cr, uid, [part1, part2], unique_fields)
-            str_unq = '---------------------------------------\n'
-            for u in unique_data:
-                for key, value in u.items():
-                    if key == 'id' or not value:
-                        continue
-                    str_unq += key + ': ' + value + '\n'
+                for i in cr.fetchall():
+                    remove_field[i[0]] = None
+                    unique_fields.append(i[0])
+                    unique_fields.append('name')
+                    unique_data = pool.get('res.partner').read(cr, uid, [part1, part2], unique_fields)
+                    str_unq = '---------------------------------------\n'
+                    for u in unique_data:
+                        for key, value in u.items():
+                            if key == 'id' or not value:
+                                continue
+                        str_unq += key + ': ' + str(value) + '\n'
+                    if res.has_key('comment') and res['comment']:
+                        res['comment'] += '\n' + str_unq
+                    else:
+                        res['comment'] = str_unq
+
         remove_field.update({'active': False})
         pool.get('res.partner').write(cr, uid, [part1, part2], remove_field)
-        if res.has_key('comment') and res['comment']:
-            res['comment'] += '\n' + str_unq
-        else:
-            res['comment'] = str_unq
+       # if res.has_key('comment') and res['comment']:
+       #     res['comment'] += '\n' + str_unq
+       # else:
+       #     res['comment'] = str_unq
 
         part_id = pool.get('res.partner').create(cr, uid, res, context)
         # For one2many fields on res.partner
         cr.execute("select name, model from ir_model_fields where relation='res.partner' and ttype not in ('many2many', 'one2many');")
         for name, model_raw in cr.fetchall():
+            print 'boum', name, model_raw
             if hasattr(pool.get(model_raw), '_auto'):
                 if not pool.get(model_raw)._auto:
                     continue

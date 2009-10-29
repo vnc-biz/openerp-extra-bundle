ce8acbcc3d5098a940f5cbc93139474cd35d398b
mra (Open ERP) <mra@tinyerp.com>
2009-10-29 16:59:10 +0530

    [FIX] account_l10nbe_domiciliation :partner view openning error for wrong customization

--- account_l10nbe_domiciliation/l10nbe_domiciliation.py
+++ account_l10nbe_domiciliation/l10nbe_domiciliation.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 from osv import fields, osv
@@ -24,10 +24,10 @@ class account_invoice(osv.osv):
     _inherit = 'account.invoice'
     _description = 'Account Invoice'
     _columns = {
-        'domiciled' : fields.boolean('Domiciled'), 
-        'domiciled_send_date' : fields.date('Domiciliation Sending Date'), 
+        'domiciled' : fields.boolean('Domiciled'),
+        'domiciled_send_date' : fields.date('Domiciliation Sending Date'),
         }
-    
+
     def on_change_partner_id(self, cr, uid, id, partner):
         if not partner:
             return {'value' : {'domiciled' : False}}
@@ -41,7 +41,7 @@ class res_partner(osv.osv):
     _inherit = 'res.partner'
     _description = 'Partner'
     _columns = {
-        'domiciliation_bool':fields.boolean('Domiciliation'), 
+        'domiciliation_bool':fields.boolean('Domiciliation'),
         'domiciliation' : fields.char('Domiciliation Number', size=32)
         }
 res_partner()
@@ -49,14 +49,15 @@ res_partner()
 class res_partner_bank(osv.osv):
     _inherit = "res.partner.bank"
     _columns = {
-                'institution_code':fields.char('Institution Code', size=3, help="Code of the financial institution used for Dom80 Export"), 
+        'institution_code':fields.char('Institution Code', size=3, help="Code of the financial institution used for Dom80 Export"),
     }
+res_partner_bank()
 
 class res_company(osv.osv):
     _inherit = 'res.company'
     _description = 'Company'
     _columns = {
-        'id_sender' : fields.char('Identification number of the Sender' , size=11, help="Sender Id for Dom80 export header"), 
+        'id_sender' : fields.char('Identification number of the Sender' , size=11, help="Sender Id for Dom80 export header"),
         'id_creditor' : fields.char('Identification number of the Creditor' , size=11, help="Creditor Id for Dom80 export header")
         }
 res_company()
@@ -67,11 +68,11 @@ class invoice_export_log(osv.osv):
     _description = "Invoice Export History"
     _rec_name = 'invoice_id'
     _columns = {
-        'state': fields.selection([('failed', 'Failed'), ('succeeded', 'Succeeded')], 'Status', readonly=True), 
-        'file': fields.binary('Saved File', readonly=True), 
-        'note': fields.text('Creation Log', readonly=True), 
-        'create_date': fields.datetime('Creation Date', required=True, readonly=True), 
-        'create_uid': fields.many2one('res.users', 'Creation User', required=True, readonly=True), 
+        'state': fields.selection([('failed', 'Failed'), ('succeeded', 'Succeeded')], 'Status', readonly=True),
+        'file': fields.binary('Saved File', readonly=True),
+        'note': fields.text('Creation Log', readonly=True),
+        'create_date': fields.datetime('Creation Date', required=True, readonly=True),
+        'create_uid': fields.many2one('res.users', 'Creation User', required=True, readonly=True),
     }
 invoice_export_log()
 

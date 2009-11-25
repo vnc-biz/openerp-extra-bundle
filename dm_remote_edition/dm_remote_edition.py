7f64b8cf50e458d1c11404467f04a0ecc0033e4c
DSH (Open ERP) <dsh@tinyerp.com>
2009-11-25 11:56:24 +0530

    [IMP] generate document function is improved with total page number qty limit

--- dm_remote_edition/dm_remote_edition.py
+++ dm_remote_edition/dm_remote_edition.py
@@ -23,7 +23,9 @@
 from osv import fields
 from osv import osv
 import pooler
-
+import re
+import base64
+pattern = re.compile("/Count\s+(\d+)")
 class dm_mail_service(osv.osv): # {{{
     _inherit = "dm.mail_service"
     
@@ -45,8 +47,7 @@ class dm_campaign_document_job(osv.osv): # {{{
     _columns = {
          'user_id': fields.many2one('res.users', 'Printer User'),
          'use_front_recap': fields.boolean('Use Front Job Recap'),
-        'use_bottom_recap': fields.boolean('Use Bottom Job Recap'),
-
+         'use_bottom_recap': fields.boolean('Use Bottom Job Recap'),
         }
   
 dm_campaign_document_job() # }}}
@@ -56,7 +57,10 @@ def generate_document_job(cr, uid, obj_id):
     camp_doc_object = pool.get('dm.campaign.document')
     obj = camp_doc_object.browse(cr,uid,[obj_id])[0]
     ms_id = obj.mail_service_id
-    camp_doc_id = camp_doc_object.search(cr,uid,[('mail_service_id','=',ms_id.id)])
+    type_id = pool.get('dm.campaign.document.type').search(cr, uid, [('code', '=', 'pdf')])[0]
+    sql = 'select id from dm_campaign_document where mail_service_id = %d and type_id=%d and campaign_document_job is null'%(ms_id.id,type_id)
+    cr.execute(sql)
+    camp_doc_id = map(lambda x: x[0] ,cr.fetchall())
     s_rule = ms_id.sorting_rule_id.by_customer_country
     camp_doc_job = {}
     if ms_id.sorting_rule_id.by_customer_country:
@@ -73,21 +77,61 @@ def generate_document_job(cr, uid, obj_id):
 			    camp_doc_job[step_id].append(camp_doc.id)
 		    else : 
 			    camp_doc_job[step_id] = [camp_doc.id]
-    print camp_doc_job
+#    elif ms_id.sorting_rule_id.by_product:
+#	    for camp_doc in camp_doc_object.browse(cr,uid,camp_doc_id):
+#		    step_id = camp_doc.document_id.step_id.id
+#		    if step_id in camp_doc_job:
+#			    camp_doc_job[step_id].append(camp_doc.id)
+#		    else : 
+#			    camp_doc_job[step_id] = [camp_doc.id]
+    elif ms_id.sorting_rule_id.by_page_qty:
+        attach_obj = pool.get('ir.attachment')
+        pattern = re.compile(r"/Count\s+(\d+)")
+        for camp_doc in camp_doc_object.browse(cr,uid,camp_doc_id):
+    	    attach_id = attach_obj.search(cr, uid,[('res_id', '=', camp_doc.id), 
+    	                           ('res_model', '=', 'dm.campaign.document')])
+            if attach_id:
+                attach = attach_obj.browse(cr, uid, attach_id[0])
+                datas = base64.decodestring(attach.datas)
+                vPages = pattern.findall(datas) and pattern.findall(datas)[0] or 0
+                if vPages in camp_doc_job :
+    			    camp_doc_job[vPages].append(camp_doc.id)
+                else : 
+    			    camp_doc_job[vPages] = [camp_doc.id]
     if camp_doc_job:
         camp_doc_job_obj = pool.get('dm.campaign.document.job')
-        for k,v in camp_doc_job.items():
-            job_id = camp_doc_job_obj.search(cr, uid, [('sorting_rule_id', '=', ms_id.sorting_rule_id.id),
-                                               ('state', '=', 'pending')])
-            if job_id:
-                job_id = job_id[0]
-            else:
-	            vals = {'name': camp_doc.segment_id.name or '' + str(k),
-					         	'user_id': ms_id.user_id,
-						        'sorting_rule_id': ms_id.sorting_rule_id.id,}
-	            job_id = camp_doc_job_obj.create(cr,uid,vals)
-            for i in v:
-                camp_doc_job_obj.write(cr, uid, job_id, {'campaign_document_ids': [[4,i]]})						 
+        job_ids = camp_doc_job_obj.search(cr, uid, [('state', '=', 'pending'),
+                            ('sorting_rule_id', '=', ms_id.sorting_rule_id.id),])
 
+        job_id = ''
+        if job_ids:
+            if ms_id.sorting_rule_id.qty_limit == 0:
+                job_id = job_ids[0]
+            else:
+                for j in camp_doc_job_obj.browse(cr, uid, job_ids):
+                    print len(j.campaign_document_ids) , ms_id.sorting_rule_id.qty_limit
+                    if len(j.campaign_document_ids) < ms_id.sorting_rule_id.qty_limit:
+                        job_id = j.id
+        if not job_id:
+            vals = {'name': camp_doc.segment_id.name or '' + str(k),
+				         	'user_id': ms_id.user_id.id,
+					        'sorting_rule_id': ms_id.sorting_rule_id.id,}
+            job_id = camp_doc_job_obj.create(cr,uid,vals)
+        doc_job = camp_doc_job_obj.browse(cr, uid, job_id)
+        number_doc = len(doc_job.campaign_document_ids)
+        for k,v in camp_doc_job.items():
+            while v:
+                if (ms_id.sorting_rule_id.qty_limit!=0 and ms_id.sorting_rule_id.qty_limit > number_doc) or ms_id.sorting_rule_id.qty_limit == 0:
+                    camp_doc_job_obj.write(cr, uid, job_id, 
+                                       {'campaign_document_ids': [[4,v.pop()]]})
+                    number_doc += 1
+                else:
+                    vals = {'name': camp_doc.segment_id.name or '' + str(k),
+    					         	'user_id': ms_id.user_id.id,
+    						        'sorting_rule_id': ms_id.sorting_rule_id.id,
+    						        'campaign_document_ids': [[4,v.pop()]]}
+                    job_id = camp_doc_job_obj.create(cr,uid,vals)
+                    doc_job = camp_doc_job_obj.browse(cr, uid, job_id)
+                    number_doc = 1
     return {'code':'doc_done','ids': obj.id}								   		    
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

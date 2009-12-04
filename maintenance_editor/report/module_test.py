edca1d0e21406f7247332cbfb08d580cf9f35a85
mra (Open ERP) <mra@tinyerp.com>
2009-12-04 12:22:47 +0530

    [IMP] maintainence_editor: small changes

--- maintenance_editor/report/module_test.py
+++ maintenance_editor/report/module_test.py
@@ -19,7 +19,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 import time
 
 from report import report_sxw
@@ -47,7 +46,7 @@ class module_test_report(report_sxw.rml_parse):
         else:
             result = ''
         return result
-    
+
     def get_state_msg(self, state, cert_no):
         state_string = ''
         if state=='done':
@@ -65,12 +64,12 @@ Please note it carefully, we strongly recommend you to even to integrate it into
         if not func_cert:
             certificate_string += '''Please note that, as requested from your side, the certificaiton process was only based on the technical quality tests. Tiny sprl disclaims any liability for failure resulting from a functional bug or error analysis.'''
         return certificate_string
-    
-    def get_data(self,line_ids):
+
+    def get_data(self, line_ids):
         category_list = []
         value_list = []
         cmp_gp = {}
-        result=[]
+        result = []
         for line in line_ids:
             res = {}
             res['category'] = line.template_id.category_id.name
@@ -97,7 +96,7 @@ Please note it carefully, we strongly recommend you to even to integrate it into
                 category_list.append(line.template_id.category_id.name)
             if line.template_id.add_value not in value_list:
                 value_list.append(line.template_id.add_value)
-        cnt_cate = 1 
+        cnt_cate = 1
         for r in result:
             cnt = 1
             if result[result.index(r)]['category']:
@@ -108,7 +107,7 @@ Please note it carefully, we strongly recommend you to even to integrate it into
                 cnt = cnt + 1
                 result[result.index(r)]['name'] = str(cnt)+") "+"Test: "+result[result.index(r)]['name']
         return result
-    
+
 report_sxw.report_sxw('report.maintenance.module.test','maintenance.maintenance.module','addons/maintenance_editor/report/module_test.rml',parser=module_test_report,header=False)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

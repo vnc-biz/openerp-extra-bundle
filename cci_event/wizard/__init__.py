1e2aa9bc709c6aeb9eef9d8c7c5b6ff654f859d0
mra (Open ERP) <mra@tinyerp.com>
2009-12-09 19:08:14 +0530

    [ADD] cci_event: wizard to create registration from partner

--- cci_event/wizard/__init__.py
+++ cci_event/wizard/__init__.py
@@ -24,5 +24,6 @@ import registration_missing_checks
 import registration_send_email
 import cci_confirm_registrations
 import create_job_registration
+import create_partner_registration
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
--- cci_event/wizard/create_partner_registration.py
+++ cci_event/wizard/create_partner_registration.py
@@ -24,11 +24,13 @@ import pooler
 
 form = """<?xml version="1.0"?>
 <form string="Create Registration">
-    <field name="event_id" width="400"/>
+    <field name="event_id" />
+    <field name="function_id" />
 </form>
 """
 fields = {
       'event_id': {'string':'Event', 'type':'many2one', 'readonly':False, 'relation': 'event.event'},
+      'function_id': {'string':'Function', 'type':'many2one', 'readonly':False, 'relation': 'res.partner.function'},
       }
 
 form_result = """<?xml version="1.0"?>
@@ -41,16 +43,23 @@ fields_result = {}
 def _create_reg(self, cr, uid, data, context={}):
     pool = pooler.get_pool(cr.dbname)
     obj_reg = pool.get('event.registration')
-    obj_job = pool.get('res.partner.job')
+    obj_fun = pool.get('res.partner.function')
+    function = obj_fun.browse(cr, uid, [data['form']['function_id']], context=context)[0].code
     obj_event = pool.get('event.event')
+    obj_part = pool.get('res.partner')
     event = data['form']['event_id']
     event = obj_event.browse(cr, uid, [event], context=context)[0]
     reg_ids = []
+    part_dict = {}
     contact_ids = []
     filter_contacts = []
-    for job in obj_job.browse(cr, uid, data['ids'], context=context):
-        contact_ids.append(job.contact_id.id)
-
+    for part in obj_part.browse(cr, uid, data['ids'], context=context):
+        for add in part.address:
+            for job in add.job_ids:
+                if job.function_code_label == function:
+                    part_dict[part.id] = job.contact_id.id
+                    contact_ids.append(job.contact_id.id)
+                    break
     cr.execute('select e.id, e.contact_id from event_registration as e  \
                 left join res_partner_contact as c on c.id=e.contact_id \
                 where e.event_id = %s and c.id in ('+','.join(map(str, contact_ids))+') \
@@ -58,21 +67,23 @@ def _create_reg(self, cr, uid, data, context={}):
     regs = cr.dictfetchall()
     if regs:
         map(lambda x: filter_contacts.append(x['contact_id']), regs)
+
     contacts_list = []
-    for job in obj_job.browse(cr, uid, data['ids'], context=context):
-        if job.contact_id.id in filter_contacts or job.contact_id.id in contacts_list:
+    for part, contact in part_dict.items():
+        if contact in filter_contacts or contact in contacts_list:
             continue
         vals = {'event_id': event.id,
-                'partner_id': job.name.id,
-                'contact_id': job.contact_id.id,
-                'invoice_label': event.product_id.name,
-                'name': 'Registration'
-                }
+        'partner_id': part,
+        'contact_id': contact,
+        'invoice_label': event.product_id.name,
+        'name': 'Registration'
+        }
         reg_id = obj_reg.create(cr, uid, vals, context=context)
+        contacts_list.append(contact)
         reg_ids.append(reg_id)
     return {'reg_ids': reg_ids}
 
-class job_registration(wizard.interface):
+class partner_registration(wizard.interface):
     def _open_reg(self, cr, uid, data, context):
         pool_obj = pooler.get_pool(cr.dbname)
         case_ids = []
@@ -105,6 +116,6 @@ class job_registration(wizard.interface):
             'result' : {'type':'action', 'action':_open_reg, 'state':'end'}
         },
     }
-job_registration("event.job_registration")
+partner_registration("event.partner_registration")
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

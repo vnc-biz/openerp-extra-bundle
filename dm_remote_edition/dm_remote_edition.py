377a3d50b0c1be3603d9e29154cdcc4d3503ff70
DSH (Open ERP) <dsh@tinyerp.com>
2009-12-22 18:29:37 +0530

    [IMP] another sorting rule and change all camp_doc

--- dm_remote_edition/dm_remote_edition.py
+++ dm_remote_edition/dm_remote_edition.py
@@ -52,121 +52,88 @@ class dm_campaign_document_job(osv.osv): # {{{
   
 dm_campaign_document_job() # }}}
 
-def generate_document_job(cr, uid, obj_id):
+def generate_document_job(cr, uid, obj_id, context):
     pool = pooler.get_pool(cr.dbname)
     camp_doc_object = pool.get('dm.campaign.document')
     obj = camp_doc_object.browse(cr,uid,[obj_id])[0]
     ms_id = obj.mail_service_id
     type_id = pool.get('dm.campaign.document.type').search(cr, uid, [('code', '=', 'pdf')])[0]
-    sql = 'select id from dm_campaign_document where mail_service_id = %d and type_id=%d and campaign_document_job is null'%(ms_id.id,type_id)
-    cr.execute(sql)
-    camp_doc_id = map(lambda x: x[0] ,cr.fetchall())
     s_rule = ms_id.sorting_rule_id.by_customer_country
     camp_doc_job = {}
+    camp_doc = camp_doc_object.browse(cr,uid,obj_id)
+    sorting_name = ''
     if ms_id.sorting_rule_id.by_customer_country:
-	    for camp_doc in camp_doc_object.browse(cr,uid,camp_doc_id):
-		    country_id = str(camp_doc.address_id.country_id.id)
-		    if country_id in camp_doc_job:
-			    camp_doc_job[country_id].append(camp_doc.id)
-		    else : 
-			    camp_doc_job[country_id] = [camp_doc.id]
+	    sorting_name = str(camp_doc.address_id.country_id.id)
     if ms_id.sorting_rule_id.by_offer_step:
-        if not camp_doc_job:
-            camp_doc_job['']=camp_doc_id
-        camp_doc_job_i = {}
-        for k,v in camp_doc_job.items() :
-            for camp_doc in camp_doc_object.browse(cr,uid,v):
-	            step_id = k+'_'+str(camp_doc.document_id.step_id.id)
-	            if step_id in camp_doc_job_i:
-		            camp_doc_job_i[step_id].append(camp_doc.id)
-	            else : 
-		            camp_doc_job_i[step_id] = [camp_doc.id]
-        camp_doc_job = camp_doc_job_i
-#    elif ms_id.sorting_rule_id.by_product:
-#	    for camp_doc in camp_doc_object.browse(cr,uid,camp_doc_id):
-#		    step_id = camp_doc.document_id.step_id.id
-#		    if step_id in camp_doc_job:
-#			    camp_doc_job[step_id].append(camp_doc.id)
-#		    else : 
-#			    camp_doc_job[step_id] = [camp_doc.id]
+        v = str(camp_doc.document_id.step_id.id)
+        sorting_name = sorting_name and sorting_name+'_'+v or v        
     if ms_id.sorting_rule_id.by_page_qty:
-        if not camp_doc_job:
-            camp_doc_job['']=camp_doc_id
-        camp_doc_job_i = {}
         attach_obj = pool.get('ir.attachment')
         pattern = re.compile(r"/Count\s+(\d+)")
-        for k,v in camp_doc_job.items() :
-            for camp_doc in camp_doc_object.browse(cr,uid,v):
-                attach_id = attach_obj.search(cr, uid,[('res_id', '=', camp_doc.id), 
-        	                           ('res_model', '=', 'dm.campaign.document')])
-                if attach_id:
-                    attach = attach_obj.browse(cr, uid, attach_id[0])
-                    datas = base64.decodestring(attach.datas)
-                    vPages = k+'_'+str(pattern.findall(datas) and pattern.findall(datas)[0] or 0)
-                else:
-                    vPages = k
-                if vPages in camp_doc_job_i :
-    			    camp_doc_job_i[vPages].append(camp_doc.id)
-                else : 
-    			    camp_doc_job_i[vPages] = [camp_doc.id]
-                    			    
-        camp_doc_job = camp_doc_job_i        			    
-    if camp_doc_job:
+        attach_id = attach_obj.search(cr, uid,[('res_id', '=', camp_doc.id), 
+	                           ('res_model', '=', 'dm.campaign.document')])
+        if attach_id:
+            attach = attach_obj.browse(cr, uid, attach_id[0])
+            datas = base64.decodestring(attach.datas)
+            v = str(pattern.findall(datas) and pattern.findall(datas)[0] or 0)
+            sorting_name = sorting_name and sorting_name+'_'+v or v
+    if ms_id.sorting_rule_id.by_product:
+        if 'workitem_id' in context:
+            res = pool.get('dm.workitem').browse(cr,uid,context['workitem_id'])
+            if res.sale_order_id:
+                product =['%d:%s'%(line.product_id.id,line.product_uom_qty) for line in res.sale_order_id.order_line]
+                product.sort()
+                v = '_'.join(product)
+                sorting_name = sorting_name and sorting_name+'_'+v or v
+    if ms_id.sorting_rule_id.by_overlay:
+        if 'segment_id' in context:
+            res = pool.get('dm.campaign.proposition.segment').browse(cr,uid,context['segment_id'])
+            if res.campaign_id:
+                product =['%d:%s'%(line.product_id.id,line.product_uom_qty) for line in res.sale_order_id.order_line]
+                product.sort()
+                v = '_'.join(product)
+                sorting_name = sorting_name and sorting_name+'_'+v or v
+                
+    if sorting_name:
         camp_doc_job_obj = pool.get('dm.campaign.document.job')
         job_ids = camp_doc_job_obj.search(cr, uid, [('state', '=', 'pending'),
-                            ('sorting_rule_id', '=', ms_id.sorting_rule_id.id),])
-
-        job_id = {}
+                            ('sorting_rule_id', '=', ms_id.sorting_rule_id.id),('sorting_name','=',sorting_name)])
+        job_id = ''
+        no_camp_doc =''
         for j_id in camp_doc_job_obj.browse(cr, uid, job_ids):
             if not ms_id.sorting_rule_id.qty_limit or ms_id.sorting_rule_id.qty_limit ==0 :
-                job_id[j_id.sorting_name] = [j_id.id, len(j_id.campaign_document_ids), 0]
+                job_id = j_id.id
             elif len(j_id.campaign_document_ids) < ms_id.sorting_rule_id.qty_limit:
-                job_id[j_id.sorting_name] = [j_id.id, len(j_id.campaign_document_ids), ms_id.sorting_rule_id.qty_limit]
-        for k,v in camp_doc_job.items():
-            while v:
-                doc_job_camp_id = v.pop()
-                if k in job_id.keys() and (job_id[k][-1] == 0 or job_id[k][1] < job_id[k][-1]) :
-                    camp_doc_job_obj.write(cr, uid, job_id[k][0], 
-                                       {'campaign_document_ids': [[4,doc_job_camp_id]]})
-                    job_id[k][1] += 1
-                else:
-                    doc_count=1
-                    camp_doc = camp_doc_object.browse(cr,uid,doc_job_camp_id)
-                    vals = {
-                            'name': camp_doc.segment_id.name or '' + str(k),
-				         	'user_id': ms_id.user_id.id,
-					        'sorting_rule_id': ms_id.sorting_rule_id.id,
-					        'campaign_document_ids': [[4,doc_job_camp_id]],
-                            'sorting_name': k,
-                            'use_front_recap': ms_id.front_job_recap and True or False,
-                            'bottom_job_recap': ms_id.bottom_job_recap and True or False,
-                            }
-                    j_id = camp_doc_job_obj.create(cr,uid,vals)
-                    if  ms_id.front_job_recap or ms_id.bottom_job_recap:
-                        doc_count = doc_count + 1
-                        if ms_id.front_job_recap:
-                            camp_vals={
-                                   'segment_id': camp_doc.segment_id.id or False,
-                                   'name': camp_doc.document_id.step_id.code + "_" + str(camp_doc.address_id.id),
-                                   'type_id': type_id,
-                                   'mail_service_id': ms_id.id,
-                                   'document_id': ms_id.front_job_recap.id,
-                                   'campaign_document_job_ids': j_id,
-                                   'address_id':camp_doc.address_id.id 
-                              }
-                            camp_document = camp_doc_object.create(cr, uid, camp_vals)
-                        if ms_id.bottom_job_recap:
-                            doc_count = doc_count + 1
-                            camp_vals={
-                                   'segment_id': camp_doc.segment_id.id or False,
-                                   'name': camp_doc.document_id.step_id.code + "_" + str(camp_doc.address_id.id),
-                                   'type_id': type_id,
-                                   'mail_service_id': ms_id.id,
-                                   'document_id': ms_id.bottom_job_recap.id,
-                                   'campaign_document_job_ids': j_id,
-                                   'address_id':camp_doc.address_id.id 
-                                  }
-                            camp_document = camp_doc_object.create(cr, uid, camp_vals)
-                    job_id[k] = [j_id, doc_count, ms_id.sorting_rule_id.qty_limit or 0]
+                job_id = j_id.id
+        if not job_id :
+            camp_doc = camp_doc_object.browse(cr,uid,obj_id)
+            job_vals = {
+                    'name': camp_doc.segment_id.name or '' + str(k),
+	             	'user_id': ms_id.user_id.id,
+		            'sorting_rule_id': ms_id.sorting_rule_id.id,
+                    'sorting_name': sorting_name,
+                    'use_front_recap': ms_id.front_job_recap and True or False,
+                    'bottom_job_recap': ms_id.bottom_job_recap and True or False,
+                    'campaign_document_ids': [[4,obj_id]],
+                    }
+            job_id = camp_doc_job_obj.create(cr,uid,job_vals)
+            if  ms_id.front_job_recap or ms_id.bottom_job_recap:
+                camp_vals={
+                       'segment_id': camp_doc.segment_id.id or False,
+                       'name': camp_doc.document_id.step_id.code + "_" + str(camp_doc.address_id.id),
+                       'type_id': type_id,
+                       'mail_service_id': ms_id.id,
+                       'address_id':camp_doc.address_id.id,
+                       'campaign_document_job_ids' : job_id
+                  }
+                if ms_id.front_job_recap: 
+                    camp_vals['document_id'] = ms_id.front_job_recap.id
+                    no_camp_doc += 1
+                if ms_id.bottom_job_recap:
+                    camp_vals['document_id'] = ms_id.bottom_job_recap.id
+                    no_camp_doc += 1                
+        else:
+            camp_doc_job_obj.write(cr, uid, job_id, 
+                                       {'campaign_document_ids': [[4,obj_id]]})
     return {'code':'doc_done','ids': obj.id}								   		    
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

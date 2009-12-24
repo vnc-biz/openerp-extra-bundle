a71b4d0e75ba155b7262d7860d50e8ce1e2a4fc3
DSH (Open ERP) <dsh@tinyerp.com>
2009-12-24 12:46:43 +0530

    [IMP] Edition and Remote edition cleaning

--- dm_remote_edition/dm_remote_edition.py
+++ dm_remote_edition/dm_remote_edition.py
@@ -22,109 +22,37 @@
 
 from osv import fields
 from osv import osv
-import pooler
-import re
-import base64
-pattern = re.compile("/Count\s+(\d+)")
+
+
 class dm_mail_service(osv.osv): # {{{
     _inherit = "dm.mail_service"
     
     _columns = {
-          'default_printer': fields.char('Default Printer', size=64),
-          'default_printer_tray': fields.char('Default Printer Tray', size=64),
           'user_id': fields.many2one('res.users', 'Printer User'),
-          'sorting_rule_id': fields.many2one('dm.campaign.document.job.sorting_rule', 'Sorting Rule'),
-          'front_job_recap': fields.many2one('dm.offer.document', 'Front Job Recap'),
-          'bottom_job_recap': fields.many2one('dm.offer.document', 'Bottom Job Recap'),
-
         }
 dm_mail_service() # }}}
-
 
 class dm_campaign_document_job(osv.osv): # {{{
     _inherit = "dm.campaign.document.job"
     
     _columns = {
          'user_id': fields.many2one('res.users', 'Printer User'),
-         'use_front_recap': fields.boolean('Use Front Job Recap'),
-         'use_bottom_recap': fields.boolean('Use Bottom Job Recap'),
         }
   
 dm_campaign_document_job() # }}}
 
-def generate_document_job(cr, uid, obj_id, context):
-    pool = pooler.get_pool(cr.dbname)
-    camp_doc_object = pool.get('dm.campaign.document')
-    obj = camp_doc_object.browse(cr,uid,[obj_id])[0]
-    ms_id = obj.mail_service_id
-    type_id = pool.get('dm.campaign.document.type').search(cr, uid, [('code', '=', 'pdf')])[0]
-    s_rule = ms_id.sorting_rule_id.by_customer_country
-    camp_doc_job = {}
-    camp_doc = camp_doc_object.browse(cr,uid,obj_id)
-    sorting_name = ''
-    if ms_id.sorting_rule_id.by_customer_country:
-	    sorting_name = str(camp_doc.address_id.country_id.id)
-    if ms_id.sorting_rule_id.by_offer_step:
-        v = str(camp_doc.document_id.step_id.id)
-        sorting_name = sorting_name and sorting_name+'_'+v or v        
-    if ms_id.sorting_rule_id.by_page_qty:
-        attach_obj = pool.get('ir.attachment')
-        pattern = re.compile(r"/Count\s+(\d+)")
-        attach_id = attach_obj.search(cr, uid,[('res_id', '=', camp_doc.id), 
-	                           ('res_model', '=', 'dm.campaign.document')])
-        if attach_id:
-            attach = attach_obj.browse(cr, uid, attach_id[0])
-            datas = base64.decodestring(attach.datas)
-            v = str(pattern.findall(datas) and pattern.findall(datas)[0] or 0)
-            sorting_name = sorting_name and sorting_name+'_'+v or v
-    if ms_id.sorting_rule_id.by_product:
-        if 'workitem_id' in context:
-            res = pool.get('dm.workitem').browse(cr,uid,context['workitem_id'])
-            if res.sale_order_id:
-                product =['%d:%s'%(line.product_id.id,line.product_uom_qty) for line in res.sale_order_id.order_line]
-                product.sort()
-                v = '_'.join(product)
-                sorting_name = sorting_name and sorting_name+'_'+v or v
-    if sorting_name:
-        camp_doc_job_obj = pool.get('dm.campaign.document.job')
-        job_ids = camp_doc_job_obj.search(cr, uid, [('state', '=', 'pending'),
-                            ('sorting_rule_id', '=', ms_id.sorting_rule_id.id),('sorting_name','=',sorting_name)])
-        job_id = ''
-        no_camp_doc =''
-        for j_id in camp_doc_job_obj.browse(cr, uid, job_ids):
-            if not ms_id.sorting_rule_id.qty_limit or ms_id.sorting_rule_id.qty_limit ==0 :
-                job_id = j_id.id
-            elif len(j_id.campaign_document_ids) < ms_id.sorting_rule_id.qty_limit:
-                job_id = j_id.id
-        if not job_id :
-            camp_doc = camp_doc_object.browse(cr,uid,obj_id)
-            job_vals = {
-                    'name': camp_doc.segment_id.name or '' + str(k),
-	             	'user_id': ms_id.user_id.id,
-		            'sorting_rule_id': ms_id.sorting_rule_id.id,
-                    'sorting_name': sorting_name,
-                    'use_front_recap': ms_id.front_job_recap and True or False,
-                    'bottom_job_recap': ms_id.bottom_job_recap and True or False,
-                    'campaign_document_ids': [[4,obj_id]],
-                    }
-            job_id = camp_doc_job_obj.create(cr,uid,job_vals)
-            if  ms_id.front_job_recap or ms_id.bottom_job_recap:
-                camp_vals={
-                       'segment_id': camp_doc.segment_id.id or False,
-                       'name': camp_doc.document_id.step_id.code + "_" + str(camp_doc.address_id.id),
-                       'type_id': type_id,
-                       'mail_service_id': ms_id.id,
-                       'address_id':camp_doc.address_id.id,
-                       'campaign_document_job_ids' : job_id
-                  }
-                if ms_id.front_job_recap: 
-                    camp_vals['document_id'] = ms_id.front_job_recap.id
-                    no_camp_doc += 1
-                if ms_id.bottom_job_recap:
-                    camp_vals['document_id'] = ms_id.bottom_job_recap.id
-                    no_camp_doc += 1                
-        else:
-            camp_doc_job_obj.write(cr, uid, job_id, 
-                                       {'campaign_document_ids': [[4,obj_id]]})
-    return {'code':'doc_done','ids': obj.id}								   		    
+
+class dm_campaign_document_job_batch(osv.osv): # {{{
+    _inherit = "dm.campaign.document.job.batch"
+    
+    def search(self, cr, uid, args, offset=0, limit=None, order=None, context=None, count=False):
+        if context is None:
+            context = {}
+        if context.has_key('portal_dm') and context['portal_dm']=='service':
+            cr.execute("select batch_id from dm_campaign_document_job where batch_id in (select id from dm_campaign_document_job_batch) and user_id = %s" %(uid))
+            batch_ids = map(lambda x: x[0], cr.fetchall())
+            return batch_ids
+        return super(dm_campaign_document_job_batch, self).search(cr, uid, args, offset, limit, order, context, count)
+
+dm_campaign_document_job_batch() # }}}			   		    
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

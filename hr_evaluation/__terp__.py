151b1cbe7485db34a77bce6733da07fb2975088e
apa-tiny <apa@tinyerp.com>
2010-01-13 17:59:19 +0530

    [IMP]:hr_evaulation:Add data for survey

--- hr_evaluation/__terp__.py
+++ hr_evaluation/__terp__.py
@@ -31,7 +31,8 @@
     "demo_xml" : ["hr_evaluation_demo.xml"],
     "update_xml" : [
 #                    "security/ir.model.access.csv",
-                    "hr_evaluation_view.xml"],
+                    "hr_evaluation_view.xml",
+                    "hr_evaluation_data.xml"],
     "active": False,
     "installable": True
 }

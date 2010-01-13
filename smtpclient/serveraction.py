82e3b3e1341b51a52709443b14b01c14bc138962
Mantavya Gajjar <mga@tinyerp.com>
2010-01-13 17:05:02 +0530

    [FIX]: body accent problem already fexed before, this fix is for the subject accent probelm

--- smtpclient/serveraction.py
+++ smtpclient/serveraction.py
@@ -63,7 +63,7 @@ class ServerAction(osv.osv):
                     logger.notifyChannel('email', netsvc.LOG_INFO, 'Partner Email address not Specified!')
                     continue
 
-                subject = self.merge_message(cr, uid, str(action.subject), action, context)
+                subject = self.merge_message(cr, uid, action.subject, action, context)
                 body = self.merge_message(cr, uid, action.message, action, context)
                 smtp_pool = self.pool.get('email.smtpclient')
 

c2f38366c9159f5df7c5d300b26a8fd59425bbcd
mra (Open ERP) <mra@tinyerp.com>
2010-01-21 18:47:11 +0530

    [IMP] cci_crm_profile: add target field on questionnaire and add domain to open selected object's questionnaire

--- cci_crm_profile/cci_crm_profile.py
+++ cci_crm_profile/cci_crm_profile.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 from osv import fields,osv
@@ -182,6 +182,13 @@ partner()
 
 class questionnaire(osv.osv):
     _inherit="crm_profiling.questionnaire"
+
+    _columns={
+        'target':fields.selection([('res.partner','Partner'),('res.partner.contact','Contact'),('res.partner.address','Address'),('res.partner.job','Function')], 'Target', required=True),
+        }
+    _defaults = {
+        'target': lambda *args: 'res.partner',
+    }
 
     def build_form(self, cr, uid, data, context):
         quest_form,quest_fields = super(questionnaire,self).build_form(cr, uid, data, context)

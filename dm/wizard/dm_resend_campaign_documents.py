345b10e66a94dd2964cc4b4a892aecfc8cc4128d
pso <pso@tinyerp.com>
2010-02-01 14:32:48 +0530

    [IMP] dm: Removed extra function

--- dm/wizard/dm_resend_campaign_documents.py
+++ dm/wizard/dm_resend_campaign_documents.py
@@ -43,27 +43,19 @@ camp_doc_fields = { # {{{
              },
     } # }}}
 
+def _resend_document(self, cr, uid, data, context):
+    pool = pooler.get_pool(cr.dbname)
+    camp_doc_obj = pool.get('dm.campaign.document')
+    wi_obj = pool.get('dm.workitem')
+    if data['form']['resend_document']:
+        camp_doc_obj.state_resent(cr, uid, data['ids'], context)
+    else:
+        camp_doc_ids = camp_doc_obj.search(cr, uid, [('state', '=', data['form']['state'])])
+        camp_doc_obj.state_resent(cr, uid, camp_doc_ids, context)
+    return {}
+
 class wizard_resend_campaign_document(wizard.interface):
     
-    def get_wi(self, cr, uid, camp_doc_obj, wi_obj, camp_doc_id):
-        camp_doc_obj.write(cr, uid, [camp_doc_id], {'state': 'resent'})
-        wi_id = camp_doc_obj.browse(cr, uid, camp_doc_id).workitem_id.id
-        if wi_id:
-            wi_obj.write(cr, uid, [wi_id], {'state': 'pending'})
-        else:
-            raise  wizard.except_wizard(_('Error'),_('Workitem is missing'))
-        
-    def _resend_document(self, cr, uid, data, context):
-        pool = pooler.get_pool(cr.dbname)
-        camp_doc_obj = pool.get('dm.campaign.document')
-        wi_obj = pool.get('dm.workitem')
-        if data['form']['resend_document']:
-            camp_doc_obj.state_resent(cr, uid, data['ids'], context)
-        else:
-            camp_doc_ids = camp_doc_obj.search(cr, uid, [('state', '=', data['form']['state'])])
-            camp_doc_obj.state_resent(cr, uid, camp_doc_ids, context)
-        return {}
-
     states = {
         'init': {
             'actions': [],

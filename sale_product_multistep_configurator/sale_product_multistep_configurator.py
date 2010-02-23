6518095cec324b3b32b8ac208b9b99f4c5381999
Jay (Open ERP) <jvo@tinyerp.com>
2010-02-23 17:43:57 +0530

    [FIX] sale_product_multistep_configurator : Minor edit

--- sale_product_multistep_configurator/sale_product_multistep_configurator.py
+++ sale_product_multistep_configurator/sale_product_multistep_configurator.py
@@ -103,7 +103,8 @@ class ir_actions_act_window(osv.osv):
     _inherit = 'ir.actions.act_window'
 
     def read(self, cr, uid, ids, fields=None, context={}, load='_classic_read'):
-
+        if not ids:
+            return {}
         if isinstance(ids, (int, long)):
             read_ids = [ids]
         else:

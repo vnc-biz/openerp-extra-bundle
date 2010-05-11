f8baa647d263c198215110506da18d4a3559d67a
DSH (Open ERP) <dsh@tinyerp.com>
2010-05-11 13:47:36 +0530

    [FIX] view of printer and mail service , search for right printer from offer document

--- dm_edition/dm_edition.py
+++ dm_edition/dm_edition.py
@@ -106,7 +106,7 @@ class dm_printer(osv.osv): # {{{
     _columns = {
         'name': fields.char('Name', size=64, required=True),
         'code': fields.char('Code', size=16, required=True),
-        'printer_model_id': fields.many2one('dm.printer.model','Printer'),
+        'printer_model_id': fields.many2one('dm.printer.model','Printer Model'),
         'mail_service_id': fields.many2one('dm.mail_service','Mail Service'),
         'note': fields.text('Description'),
 
@@ -119,7 +119,7 @@ class dm_printer_tray(osv.osv): # {{{
     _columns = {
         'name': fields.char('Name', size=64, required=True),
         'code': fields.char('Code', size=16, required=True),
-        'printer_tray_model_id' : fields.many2one('dm.printer_tray.model','Printer Tray'),
+        'printer_tray_model_id' : fields.many2one('dm.printer_tray.model','Printer Tray Model'),
         'printer_id' : fields.many2one('dm.printer','Printer'),
         'note': fields.text('Description'),
 
@@ -146,6 +146,7 @@ class dm_mail_service(osv.osv): # {{{
           'sorting_rule_id': fields.many2one('dm.campaign.document.job.sorting_rule', 'Sorting Rule'),
           'front_job_recap': fields.many2one('dm.offer.document', 'Front Job Recap'),
           'bottom_job_recap': fields.many2one('dm.offer.document', 'Bottom Job Recap'),
+          'user_id': fields.many2one('res.users', 'Printer User'),
         }
     
 dm_mail_service() # }}}
@@ -166,22 +167,26 @@ dm_campaign_document() # }}}
 def generate_document_job(cr, uid, obj_id, context):
     pool = pooler.get_pool(cr.dbname)
     camp_doc_object = pool.get('dm.campaign.document')
-    obj = camp_doc_object.browse(cr,uid,[obj_id])[0]
-    ms_id = obj.mail_service_id
-    type_id = pool.get('dm.campaign.document.type').search(cr, uid, [('code', '=', 'pdf')])[0]
-    s_rule = ms_id.sorting_rule_id.by_customer_country
     camp_doc = camp_doc_object.browse(cr,uid,obj_id)
+    ms_id = camp_doc.mail_service_id
+    type_id = pool.get('dm.campaign.document.type').search(cr, uid, [('code', '=', 'pdf')])
+    type_id = type_id and type_id[0] or ''
     sorting_name = ''
-    offer_doc_obj = pool.get('dm.offer.document').browse(cr,uid,[obj.document_id.id])[0]
-    printer_tray_model_id = offer_doc_obj.printer_tray_model_id.id
+    offer_doc_obj = pool.get('dm.offer.document').browse(cr,uid,camp_doc.document_id.id)
+    printer_tray_id = ''
     
-    if printer_tray_model_id:
-        printer_tray_obj =pool.get('dm.printer_tray').browse(cr, uid, printer_tray_model_id)
-        printer_tray_id = printer_tray_obj.id
-        printer_id = printer_tray_obj.printer_id.id
-    else:
+    if offer_doc_obj.printer_tray_model_id:
+        printer_tray_id = pool.get('dm.printer_tray').search(cr, 
+                    uid,
+                    [('printer_tray_model_id','=',offer_doc_obj.printer_tray_model_id.id)])
+        if printer_tray_id :
+            printer_tray_id = printer_tray_id[0]
+            printer_tray_obj = pool.get('dm.printer_tray').browse(cr, uid, printer_tray_id) 
+            printer_id = printer_tray_obj.printer_id.id
+    if not printer_tray_id:
         printer_tray_id = ms_id.default_printer_tray_id.id
         printer_id = ms_id.default_printer_id.id
+        
     camp_doc_object.write(cr, uid, [obj_id], {'printer_tray_id': printer_tray_id,
                                               'printer_id': printer_id})
     
@@ -230,7 +235,6 @@ def generate_document_job(cr, uid, obj_id, context):
             elif len(j_id.campaign_document_ids) < ms_id.sorting_rule_id.qty_limit:
                 job_id = j_id.id
         if not job_id :
-            camp_doc = camp_doc_object.browse(cr,uid,obj_id)
             job_vals = {
                     'name': (camp_doc.segment_id and camp_doc.segment_id.name or '') + str(sorting_name),
 	             	'user_id': ms_id.user_id.id,
@@ -248,8 +252,24 @@ def generate_document_job(cr, uid, obj_id, context):
                        'type_id': type_id,
                        'mail_service_id': ms_id.id,
                        'address_id': camp_doc.address_id.id,
-                       'campaign_document_job': job_id
+                       'campaign_document_job': job_id,
+                       'printer_tray_id': printer_tray_id,
+                       'printer_id': printer_id,
                   }
+                if not 'address_id' in context :
+                    context['address_id'] = camp_doc.address_id.id
+                if not 'document_id' in context :
+                    context['document_id'] = camp_doc.document_id.id
+                if not 'store_document' in context :
+                    context['store_document'] = ms_id.store_document
+                if not 'workitem_id' in context :
+                    context['workitem_id'] = camp_doc.workitem_id.id
+                if not 'step_id' in context :
+                    context['step_id'] = camp_doc.workitem_id.step_id.id
+                if not 'segment_id' in context :
+                    context['segment_id'] = camp_doc.workitem_id.segment_id.id             
+                # reports for the front and bottom job should be saved as 
+                # attachment of the campaign document    ?
                 if ms_id.front_job_recap: 
                     camp_vals['document_id'] = ms_id.front_job_recap.id
                     new_camp_id = camp_doc_object.create(cr,uid,camp_vals)
@@ -271,7 +291,7 @@ def generate_document_job(cr, uid, obj_id, context):
         else:
           camp_doc_job_obj.write(cr, uid, job_id, 
                                        {'campaign_document_ids': [[4,obj_id]]})
-    return {'code':'doc_done','ids': obj.id}		
+    return {'code':'doc_done','ids': camp_doc.id}		
 
 	
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

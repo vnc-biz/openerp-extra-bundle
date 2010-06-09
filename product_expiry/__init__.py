2e86ab5613089666cdec72efc067e965adace039
nel@tinyerp.com <>
2010-06-09 13:06:13 +0200

    import wizard

--- product_expiry/__init__.py
+++ product_expiry/__init__.py
@@ -19,5 +19,6 @@
 ##############################################################################
 
 import product_expiry
+import wizard
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 

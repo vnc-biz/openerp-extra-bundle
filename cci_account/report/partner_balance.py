110a99c8e6b4fb3282b094d78cba161c7c08d16d
Fabien Pinckaers <fp@tinyerp.com>
2010-07-22 14:06:08 +0200

    fix

--- cci_account/report/partner_balance.py
+++ cci_account/report/partner_balance.py
@@ -40,35 +40,10 @@ class partner_balance(report_sxw.rml_parse):
         })
 
     def set_context(self, objects, data, ids, report_type = None):
-        partner_category_obj = pooler.get_pool(self.cr.dbname).get('res.partner.category')
-        if data['form']['category'] == 'Customer' or data['form']['category'] == 'Supplier' :
-            cat_id=partner_category_obj.search(self.cr,self.uid,[('name','=',data['form']['category'])])
-            cat_id+=partner_category_obj.search(self.cr,self.uid,[('parent_id','child_of',cat_id)])
-        else:
-            cat_id=partner_category_obj.search(self.cr,self.uid,[('name','in',("Supplier","Customer"))])
-            cat_id+=partner_category_obj.search(self.cr,self.uid,[('parent_id','child_of',cat_id)])
-        self.cr.execute('SELECT partner_id from res_partner_category_rel where category_id in ('+','.join(map(str,cat_id))+')')
-        data_parner=self.cr.fetchall()
-        self.par_ids=[]
-        self.par_ids=list(set([x[0] for x in data_parner]))
-
         account_move_line_obj = pooler.get_pool(self.cr.dbname).get('account.move.line')
         line_query = account_move_line_obj._query_get(self.cr, self.uid, obj='line',
                 context={'fiscalyear': data['form']['fiscalyear']})
-        self.cr.execute('SELECT DISTINCT line.partner_id ' \
-                'FROM account_move_line AS line, account_account AS account ' \
-                'WHERE line.partner_id IS NOT NULL ' \
-                    'AND line.date >= %s ' \
-                    'AND line.date <= %s ' \
-                    'AND ' + line_query + ' ' \
-                    'AND line.account_id = account.id ' \
-                    'AND account.company_id = %s ' \
-                    "AND partner_id in ("+','.join(map(str,self.par_ids))+") " \
-                    'AND account.active ',
-                    (data['form']['date1'], data['form']['date2'],
-                        data['form']['company_id']))
-        new_ids = [id for (id,) in self.cr.fetchall()]
-        #
+
         if (data['form']['category'] == 'Customer' ):
             self.ACCOUNT_TYPE = "('receivable')"
         elif (data['form']['category'] == 'Supplier'):
@@ -84,6 +59,20 @@ class partner_balance(report_sxw.rml_parse):
                     "AND a.type IN " + self.ACCOUNT_TYPE + " " \
                     "AND a.active", (data['form']['company_id'],))
         self.account_ids = ','.join([str(a) for (a,) in self.cr.fetchall()])
+
+        self.cr.execute('SELECT DISTINCT line.partner_id ' \
+                'FROM account_move_line AS line, account_account AS account ' \
+                'WHERE line.partner_id IS NOT NULL ' \
+                    'AND line.date >= %s ' \
+                    'AND line.date <= %s ' \
+                    'AND ' + line_query + ' ' \
+                    'AND line.account_id in ('+self.account_ids+') ' \
+                    'AND account.company_id = %s ' \
+                    'AND account.active ',
+                    (data['form']['date1'], data['form']['date2'],
+                        data['form']['company_id']))
+        new_ids = [id for (id,) in self.cr.fetchall()]
+
         self.partner_ids = ','.join(map(str, new_ids))
         objects = self.pool.get('res.partner').browse(self.cr, self.uid, new_ids)
         super(partner_balance, self).set_context(objects, data, new_ids, report_type)
@@ -113,8 +102,9 @@ class partner_balance(report_sxw.rml_parse):
                             "AND " + line_query + " " \
                     ") AS enlitige " \
             "FROM account_move_line l LEFT JOIN res_partner p ON (l.partner_id=p.id) " \
-            "WHERE partner_id IN (" + self.partner_ids + ") " \
-                "AND account_id IN (" + self.account_ids + ") " \
+            "WHERE "
+                "account_id IN (" + self.account_ids + ") " \
+                "AND partner_id is not null " \
                 "AND l.date >= %s " \
                 "AND l.date <= %s " \
                 "AND " + line_query + " " \
@@ -135,8 +125,9 @@ class partner_balance(report_sxw.rml_parse):
         self.cr.execute(
                 'SELECT sum(debit) ' \
                 'FROM account_move_line ' \
-                'WHERE partner_id IN (' + self.partner_ids + ') ' \
-                    'AND account_id IN (' + self.account_ids + ') ' \
+                'WHERE ' \
+                    'account_id IN (' + self.account_ids + ') ' \
+                    "AND partner_id is not null " \
                     'AND date >=%s ' \
                     'AND date <= %s ' \
                     'AND ' + line_query,
@@ -153,8 +144,9 @@ class partner_balance(report_sxw.rml_parse):
         self.cr.execute(
                 'SELECT sum(credit) ' \
                 'FROM account_move_line ' \
-                'WHERE partner_id IN (' + self.partner_ids + ') ' \
-                    'AND account_id IN (' + self.account_ids + ') ' \
+                'WHERE '
+                    'account_id IN (' + self.account_ids + ') ' \
+                    "AND partner_id is not null " \
                     'AND date >= %s ' \
                     'AND date <= %s ' \
                     'AND ' + line_query,
@@ -171,8 +163,9 @@ class partner_balance(report_sxw.rml_parse):
         self.cr.execute(
                 'SELECT sum(debit-credit) ' \
                 'FROM account_move_line ' \
-                'WHERE partner_id IN (' + self.partner_ids + ') ' \
-                    'AND account_id IN (' + self.account_ids + ') ' \
+                'WHERE ' \
+                    'account_id IN (' + self.account_ids + ') ' \
+                    "AND partner_id is not null " \
                     'AND date >= %s ' \
                     'AND date <= %s ' \
                     'AND blocked=TRUE ' \
@@ -193,8 +186,9 @@ class partner_balance(report_sxw.rml_parse):
                     'ELSE 0 ' \
                 'END ' \
             'FROM account_move_line ' \
-            'WHERE partner_id IN (' + self.partner_ids + ') ' \
-                'AND account_id IN (' + self.account_ids + ') ' \
+            'WHERE ' \
+                'account_id IN (' + self.account_ids + ') ' \
+                "AND partner_id is not null " \
                 'AND date >= %s ' \
                 'AND date <= %s ' \
                 'AND ' + line_query + ' ' \
@@ -215,8 +209,9 @@ class partner_balance(report_sxw.rml_parse):
                     'ELSE 0 ' \
                 'END ' \
             'FROM account_move_line ' \
-            'WHERE partner_id IN (' + self.partner_ids + ') ' \
-                'AND account_id IN (' + self.account_ids + ') ' \
+            'WHERE '
+                'account_id IN (' + self.account_ids + ') ' \
+                "AND partner_id is not null " \
                 'AND date >= %s ' \
                 'AND date <= %s ' \
                 'AND ' + line_query + ' ' \

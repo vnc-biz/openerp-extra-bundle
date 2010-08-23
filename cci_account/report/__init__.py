b4646ce0c10b03967ae4b6a7251f7b470c6dbdd7
nel@tinyerp.com <>
2010-08-23 14:35:15 +0200

    [ADD] report statement

--- cci_account/report/__init__.py
+++ cci_account/report/__init__.py
@@ -25,5 +25,6 @@ import cci_draft_invoice_info
 import cci_vcs_report
 import report_account_invoice_layout
 import special_message_invoice
+import account_statement
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 

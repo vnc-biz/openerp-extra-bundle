14132747a0edc853dc148c512517b636c3572ce5
sebastien.beau <>
2010-09-28 01:08:35 -0300

    [REF] extract the function execute_python_code and execute_transformation from the function start_kettle_tranformation

--- kettle_connector/kettle.py
+++ kettle_connector/kettle.py
@@ -41,6 +41,45 @@ class kettle_transformation(osv.osv):
         'python_code_after' : fields.text('Python Code Executed After Transformation'),
     }
     
+    def execute_python_code(self, cr, uid, id, position, context):
+        logger = netsvc.Logger()
+        transfo = self.read(cr, uid, id, ['active_python_code', 'python_code_' + position], context)
+        if transfo['active_python_code'] and transfo['python_code_' + position]:
+            logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "execute python code " + position +" kettle transformation")
+            exec(transfo['python_code_' + position])
+            logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "python code executed")
+        return context
+    
+    def error_wizard(self, cr, uid, kettle_dir, context):
+        error_file = open(kettle_dir+'/nohup.out', 'r')
+        error_log = error_file.readlines()
+        end_error_log = ''
+        for i in range(0,20):
+            end_error_log = error_log.pop() + end_error_log
+        raise osv.except_osv('Error !', 'You have an error in your kettle transformation, please look at the kettle log in ' + str(kettle_dir) + '/nohup.out' + "\n \n" + str(end_error_log))
+          
+    def execute_transformation(self, cr, uid, id, filter, context):
+        transfo = self.read(cr, uid, id, ['kettle_dir','file_name'], context)
+        logger = netsvc.Logger()
+        file = transfo['kettle_dir']+'/transformations/'+transfo['file_name']
+        if not os.path.isfile(file + '.ktr'):
+            raise osv.except_osv('Error !', 'The transformation file or the kettle directory is invalid')
+        csv_file = open(file +'.ktr', 'r')
+        csv_temp = open(file +'_temp.ktr', 'w')
+
+        for line in csv_file.readlines():
+            for key in filter:
+                line = line.replace(key, filter[key])
+            csv_temp.write(line)
+        csv_temp.close()
+        
+        logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "start kettle transformation : kettle log in " + str(transfo['kettle_dir']) + '/nohup.out')
+        cmd = "cd " + transfo['kettle_dir'] + "; rm nohup.out ; nohup sh pan.sh -file=transformations/" + transfo['file_name'] + '_temp.ktr'
+        
+        if os.system(cmd) != 0:
+            self.error_wizard(cr, uid, transfo['kettle_dir'], context)
+        logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "kettle transformation finish with success")
+   
 kettle_transformation()
 
 class kettle_wizard(osv.osv_memory):
@@ -60,59 +99,27 @@ class kettle_wizard(osv.osv_memory):
         'upload_file': _get_add_file,
     }
 
-    def error_wizard(self, cr, uid, kettle_dir, context):
-        error_file = open(kettle_dir+'/nohup.out', 'r')
-        error_log = error_file.readlines()
-        end_error_log = ''
-        for i in range(0,20):
-            end_error_log = error_log.pop() + end_error_log
-        raise osv.except_osv('Error !', 'You have an error in your kettle transformation, please look at the kettle log in ' + str(kettle_dir) + '/nohup.out' + "\n \n" + str(end_error_log))
-        
-        
     def start_kettle_tranformation(self, cr, uid, ids, context=None):
         logger = netsvc.Logger()
         user = self.pool.get('res.users').browse(cr, uid, uid, context)
+        obj_transfo = self.pool.get('kettle.transformation')
         for id in ids:
             filter = {'AUTO_REP_db_erp': str(cr.dbname), 'AUTO_REP_user_erp': str(user.login), 'AUTO_REP_db_pass_erp': str(user.password)}
-        
-            transfo = self.pool.get('kettle.transformation').browse(cr, uid, id, context)
-            if transfo.upload_file: 
+            transfo = obj_transfo.read(cr, uid, id, ['upload_file', 'parameters'], context)
+            if transfo['upload_file']: 
                 if not (context and context.get('input_filename',False)):
                     logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "the transformation " + transfo.name + " can't be executed because the anyone File was uploaded")
                     continue
                 else:
                     filter.update({'AUTO_REP_file_in' : str(context['input_filename'])})
-                
-            if transfo.active_python_code and transfo.python_code_before:
-                logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "execute python code before kettle transformation")
-                exec(transfo.python_code_before)
-                logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "python code executed")
             
-            file = transfo.kettle_dir+'/transformations/'+transfo.file_name+'.ktr'
-            if not os.path.isfile(file):
-                raise osv.except_osv('Error !', 'The tranformastion file or the kettle directory is invalid')
-            csv_file = open(transfo.kettle_dir+'/transformations/'+transfo.file_name+'.ktr', 'r')
-            csv_temp = open(transfo.kettle_dir+'/transformations/'+transfo.file_name+'_temp.ktr', 'w')
+            context = obj_transfo.execute_python_code(cr, uid, id, 'before', context)
             
-            filter.update(eval('{' + str(transfo.parameters or '')+ '}'))
+            filter.update(eval('{' + str(transfo['parameters'] or '')+ '}'))
+            obj_transfo.execute_transformation(cr, uid, id, filter, context)
 
-            for line in csv_file.readlines():
-                for key in filter:
-                    line = line.replace(key, filter[key])
-                csv_temp.write(line)
-            csv_temp.close()
-            
-            logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "start kettle transformation : kettle log in " + str(transfo.kettle_dir) + '/nohup.out')
-            cmd = "cd " + transfo.kettle_dir + "; rm nohup.out ; nohup sh pan.sh -file=transformations/" + transfo.file_name + '_temp.ktr'
-            
-            if os.system(cmd) != 0:
-                self.error_wizard(cr, uid, transfo.kettle_dir, context)
-            logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "kettle transformation finish with success")
-            
-            if transfo.active_python_code and transfo.python_code_after:
-                logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "execute python code after kettle transformation")
-                exec(transfo.python_code_after)
-                logger.notifyChannel('kettle-connector', netsvc.LOG_INFO, "python code executed")
+            context = obj_transfo.execute_python_code(cr, uid, id, 'after', context)
+                
         return True
 
 

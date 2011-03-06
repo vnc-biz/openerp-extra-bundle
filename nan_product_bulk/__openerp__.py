3718cdeb0a9df5ed0be2fba9eca6a0747c714d86
Albert Cervera i Areny <albert@nan-tic.com>
2011-03-07 00:27:11 +0100

    [ADD] nan_product_bulk: Add new nan_product_bulk module.

---
+++ nan_product_bulk/__openerp__.py
@@ -0,0 +1,14 @@
+{
+	"name" : "Product Bulk",
+	"version" : "0.1",
+	"description" : """Product Bulk""",
+	"author" : "NaN for Trod y Avia, S.L.",
+	"website" : "http://www.NaN-tic.com",
+	"depends" : [ 'stock','mrp'], 
+	"category" : "Custom Modules",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : [ 'product_view.xml' ],
+	"active": False,
+	"installable": True
+}

ae6fa246155e649863e9ff0b4876fe9edd3cd91d
eLBati <lorenzo.battistini@agilebg.com>
2011-03-24 18:48:36 +0100

    FIX sale_intercompany_auto

--- sale_intercompany_auto/sale_interco.py
+++ sale_intercompany_auto/sale_interco.py
@@ -34,50 +34,50 @@ class purchase_order(osv.osv):
                 sale_line_obj = self.pool.get('sale.order.line')
                 partner_obj = self.pool.get('res.partner')
 
-                shop_id = self.pool.get('sale.shop').search(cr, uid, [])[0]
+                shop_ids = self.pool.get('sale.shop').search(cr, uid, [('company_id', '=', cids[0])])
 
+                if shop_ids:
+                    shop_id = shop_ids[0]
+                    new_ids = []
 
-                new_ids = []
-
-                user = self.pool.get('res.users').browse(cr, uid, uid)
-                partner_id = user.company_id.partner_id.id
-                partner_addr = partner_obj.address_get(cr, uid, [partner_id],
-                        ['invoice', 'delivery', 'contact'])
-                default_pricelist = partner_obj.browse(cr, uid, partner_id,
-                            {}).property_product_pricelist.id
-                fpos = partner_obj.browse(cr, uid, partner_id,
-                            {}).property_account_position
-                fpos_id = fpos and fpos.id or False
-                vals = {
-                    'origin': 'PO:%s' % str(po.name),
-                    'picking_policy': 'direct',
-                    'shop_id': shop_id,
-                    'partner_id': partner_id,
-                    'pricelist_id': default_pricelist,
-                    'partner_invoice_id': partner_addr['invoice'],
-                    'partner_order_id': partner_addr['contact'],
-                    'partner_shipping_id': partner_addr['delivery'],
-                    'order_policy': 'manual',
-                    'date_order': time.strftime('%Y-%m-%d'),
-                    'order_policy': po.invoice_method=='picking' and 'picking' or 'manual',
-                    'fiscal_position': fpos_id
-                }
-                new_id = sale_obj.create(cr, uid, vals)
-                fpos = user.company_id.partner_id.property_account_position and user.company_id.partner_id.property_account_position.id or False
-                for line in po.order_line:
-                    value = sale_line_obj.product_id_change(cr, uid, [], default_pricelist,
-                            line.product_id.id, qty=line.product_qty, partner_id=partner_id, fiscal_position=fpos)['value']
-                    value['price_unit'] = line.price_unit
-                    value['product_id'] = line.product_id.id
-                    value['product_uos'] = value.get('product_uos', False)
-                    value['product_uom_qty'] = line.product_qty
-                    value['order_id'] = new_id
-                    value['type'] = 'make_to_stock'
-                    sale_line_obj.create(cr, uid, value)
-                wf_service = netsvc.LocalService("workflow")
-                wf_service.trg_validate(uid, 'sale.order', new_id, 'order_confirm', cr)
-                wf_service.trg_validate(uid, 'sale.order', new_id, 'manual_invoice', cr)
-
+                    user = self.pool.get('res.users').browse(cr, uid, uid)
+                    partner_id = user.company_id.partner_id.id
+                    partner_addr = partner_obj.address_get(cr, uid, [partner_id],
+                            ['invoice', 'delivery', 'contact'])
+                    default_pricelist = partner_obj.browse(cr, uid, partner_id,
+                                {}).property_product_pricelist.id
+                    fpos = partner_obj.browse(cr, uid, partner_id,
+                                {}).property_account_position
+                    fpos_id = fpos and fpos.id or False
+                    vals = {
+                        'origin': 'PO:%s' % str(po.name),
+                        'picking_policy': 'direct',
+                        'shop_id': shop_id,
+                        'partner_id': partner_id,
+                        'pricelist_id': default_pricelist,
+                        'partner_invoice_id': partner_addr['invoice'],
+                        'partner_order_id': partner_addr['contact'],
+                        'partner_shipping_id': partner_addr['delivery'],
+                        'order_policy': 'manual',
+                        'date_order': time.strftime('%Y-%m-%d'),
+                        'order_policy': po.invoice_method=='picking' and 'picking' or 'manual',
+                        'fiscal_position': fpos_id
+                    }
+                    new_id = sale_obj.create(cr, uid, vals)
+                    fpos = user.company_id.partner_id.property_account_position and user.company_id.partner_id.property_account_position.id or False
+                    for line in po.order_line:
+                        value = {}
+                        if line.product_id:
+                            value = sale_line_obj.product_id_change(cr, uid, [], default_pricelist,
+                                    line.product_id.id, qty=line.product_qty, partner_id=partner_id, fiscal_position=fpos)['value']
+                            value['product_id'] = line.product_id.id
+                            value['product_uos'] = value.get('product_uos', False)
+                        value['name'] = line.name
+                        value['price_unit'] = line.price_unit
+                        value['product_uom'] = line.product_uom.id
+                        value['product_uom_qty'] = line.product_qty
+                        value['order_id'] = new_id
+                        sale_line_obj.create(cr, uid, value)
         return res
 purchase_order()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

2ddac5f9d99780f576bfbf265a818e213fdfb584
Christophe Chauvet <christophe.chauvet@syleam.fr>
2011-04-14 10:50:52 +0200

    [IMP][network] PEP8 compliawnt

--- network/network.py
+++ network/network.py
@@ -1,9 +1,10 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution	
-#    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    $Id$
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    Copyright (C) 2010-2011 OpenERP SA (<http://openerp.com>). All Rights Reserved
+#    Copyright (C) 2011 SYLEAM (<http://syleam.fr>). Christophe CHAUVET
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -21,50 +22,60 @@
 ##############################################################################
 
 import time
-from osv import fields, osv
+from osv import osv
+from osv import fields
 
-#---------------------------------------------------------
-# Type of hardware: Printers, Screens, HD, ....
-#---------------------------------------------------------
+
 class network_hardware_type(osv.osv):
+    """
+    Type of hardware: Printers, Screens, HD, ....
+    """
     _name = "network.hardware.type"
     _description = "Hardware type"
+
     _columns = {
         'name': fields.char('Type of material', size=64, translate=True, required=True),
         'networkable': fields.boolean('Networkable hardware'),
     }
+
     _defaults = {
         'networkable': lambda *a: False,
     }
+
 network_hardware_type()
 
-#--------------------------------------------------------------
-# A network is composed of all kind of networkable materials
-#--------------------------------------------------------------
+
 class network_network(osv.osv):
+    """
+    A network is composed of all kind of networkable materials
+    """
     _name = 'network.network'
     _description = 'Network'
+
     _columns = {
         'name': fields.char('Network name', size=64, required=True),
         'range': fields.char('Address range', size=128),
-        'user_id': fields.many2one('res.users','Onsite Contact person'),
+        'user_id': fields.many2one('res.users', 'Onsite Contact person'),
         'contact_id': fields.many2one('res.partner', 'Partner', required=True),
         'material_ids': fields.one2many('network.material', 'network_id', 'Members'),
     }
 
 network_network()
 
+
 def _calc_warranty(*args):
     now = list(time.localtime())
     now[0] += 1
     return time.strftime('%Y-%m-%d', tuple(now))
 
-#----------------------------------------------------------
-# Materials; computer, printer, switch, ...
-#----------------------------------------------------------
+
 class network_material(osv.osv):
+    """
+    # Materials; computer, printer, switch, ...
+    """
     _name = "network.material"
     _description = "Material"
+
     _columns = {
         'name': fields.char('Device Name', size=64, required=True),
         'ip_addr': fields.char('IP Address', size=16),
@@ -77,27 +88,31 @@ class network_material(osv.osv):
         'note': fields.text('Notes'),
         'parent_id': fields.many2one('network.material',
                                      'Parent Material'),
-        'child_id': fields.one2many('network.material', 'parent_id', 
+        'child_id': fields.one2many('network.material', 'parent_id',
                                     'Childs Materials'),
         'software_id': fields.one2many('network.software',
-                                       'material_id', 
+                                       'material_id',
                                        'Installed Software'),
         'change_id': fields.one2many('network.changes',
                                      'machine_id',
                                      'Changes on this machine'),
     }
+
     _defaults = {
          'date': lambda *a: time.strftime('%Y-%m-%d'),
          'warranty': _calc_warranty,
     }
+
 network_material()
 
-#----------------------------------------------------------
-# Changes on this machine
-#----------------------------------------------------------
+
 class network_changes(osv.osv):
+    """
+    History all changes on this machine
+    """
     _name = 'network.changes'
     _description = 'Network changes'
+    _order = 'date desc'
 
     _columns = {
         'name': fields.char('Short Description', size=64,
@@ -111,31 +126,34 @@ class network_changes(osv.osv):
 
     _defaults = {
         'date': lambda *a: time.strftime('%Y-%m-%d %H:%M:%S'),
-        'user_id': lambda self,cr,uid,ctx : uid,
+        'user_id': lambda self, cr, uid, ctx: uid,
     }
-
-    _order = 'date desc'
 
 network_changes()
 
-#----------------------------------------------------------
-# Type of Software; LDAP, Tiny ERP, Postfix
-#----------------------------------------------------------
+
 class network_soft_type(osv.osv):
+    """
+    Type of Software; LDAP, Tiny ERP, Postfix
+    """
     _name = "network.software.type"
     _description = "Software type"
+
     _columns = {
         'name': fields.char('Composant Name', size=64, translate=True, required=True),
         'note': fields.text('Notes'),
     }
+
 network_soft_type()
 
-#----------------------------------------------------------
-# A software installed on a material
-#----------------------------------------------------------
+
 class network_software(osv.osv):
+    """
+    A software installed on a material
+    """
     _name = "network.software"
     _description = "Software"
+
     _columns = {
         'name': fields.char('Composant Name', size=64, required=True),
         'material_id': fields.many2one('network.material', 'Material'),
@@ -148,21 +166,25 @@ class network_software(osv.osv):
         'date': fields.date('Installation Date', size=32),
         'note': fields.text('Notes'),
     }
+
 network_software()
 
-#------------------------------------------------------------
-# Couples of login/password
-#------------------------------------------------------------
+
 class network_software_logpass(osv.osv):
+    """
+    Couples of login/password
+    """
     _name = "network.software.logpass"
     _description = "Software login"
     _rec_name = 'login'
+
     _columns = {
         'login': fields.char('Login', size=64, required=True),
         'password': fields.char('Password', size=64, required=True),
         'software_id': fields.many2one('network.software',
                                     'Software', required=True),
     }
-network_software_logpass()
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
+network_software_logpass()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

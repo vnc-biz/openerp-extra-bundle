1b0c2a35be2228bad2ee4627b8224a4bd8557f8e
renatonlima@gmail.com <>
2011-05-26 21:51:59 -0300

    account_fiscal_position_rule_purchase: fix method onchange_partner_id in purchase.order object

--- account_fiscal_position_rule_purchase/purchase.py
+++ account_fiscal_position_rule_purchase/purchase.py
@@ -1,9 +1,7 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
+#################################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    $Id$
+#    Copyright (C) 2010  Renato Lima - Akretion
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -18,13 +16,10 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+#################################################################################
 
 from osv import fields, osv
 
-##############################################################################
-# Ordem de Compra Customizada
-##############################################################################
 class purchase_order(osv.osv):
     _inherit = 'purchase.order'
     
@@ -35,15 +30,15 @@ class purchase_order(osv.osv):
         if not part or not company_id or not result['value']['partner_address_id']:
             return result
 
-        obj_company = self.pool.get('res.company').browse(cr, uid, [company_id])[0]
+        obj_company = self.pool.get('res.company').browse(cr, uid, company_id)
 
         company_addr = self.pool.get('res.partner').address_get(cr, uid, [obj_company.partner_id.id], ['default'])
-        company_addr_default = self.pool.get('res.partner.address').browse(cr, uid, [company_addr['default']])[0]
+        company_addr_default = self.pool.get('res.partner.address').browse(cr, uid, company_addr['default'])
 
         from_country = company_addr_default.country_id.id
         from_state = company_addr_default.state_id.id
 
-        obj_partner = self.pool.get('res.partner').browse(cr, uid, [part])[0]
+        obj_partner = self.pool.get('res.partner').browse(cr, uid, part)
         if obj_partner.property_account_position:
             result['value']['fiscal_position'] = obj_partner.property_account_position
             return result
@@ -57,21 +52,19 @@ class purchase_order(osv.osv):
         
         if fsc_pos_id:
             obj_fpo_rule = self.pool.get('account.fiscal.position.rule').browse(cr, uid, fsc_pos_id)[0]
-            obj_fpo = self.pool.get('account.fiscal.position').browse(cr, uid, [obj_fpo_rule.fiscal_position_id.id])[0]
-            obj_foperation = self.pool.get('l10n_br.fiscal.operation').browse(cr, uid, [obj_fpo.fiscal_operation_id.id])[0]
-            result['value']['fiscal_position'] = obj_fpo.id
-            
+            result['value']['fiscal_position'] = obj_fpo_rule.fiscal_position_id.id
+
         return result
 
     def onchange_partner_address_id(self, cr, uid, ids, partner_address_id, company_id=False):
 
-	result = {'value': {}}
+        result = {'value': {}}
 
         if not partner_address_id or not company_id:
 	    result = {'value': {'fiscal_position': False}}
             return result
 
-        obj_company = self.pool.get('res.company').browse(cr, uid, [company_id])[0]
+        obj_company = self.pool.get('res.company').browse(cr, uid, company_id)
 
         company_addr = self.pool.get('res.partner').address_get(cr, uid, [obj_company.partner_id.id], ['default'])
         company_addr_default = self.pool.get('res.partner.address').browse(cr, uid, [company_addr['default']])[0]
@@ -93,9 +86,7 @@ class purchase_order(osv.osv):
         
         if fsc_pos_id:
             obj_fpo_rule = self.pool.get('account.fiscal.position.rule').browse(cr, uid, fsc_pos_id)[0]
-            obj_fpo = self.pool.get('account.fiscal.position').browse(cr, uid, [obj_fpo_rule.fiscal_position_id.id])[0]
-            obj_foperation = self.pool.get('l10n_br.fiscal.operation').browse(cr, uid, [obj_fpo.fiscal_operation_id.id])[0]
-            result['value']['fiscal_position'] = obj_fpo.id
+            result['value']['fiscal_position'] = obj_fpo_rule.fiscal_position_id.id
             
         return result
 

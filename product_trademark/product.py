93b57de35701fd18629303f96b2233a3fffacb12
Jordi Esteve <jesteve@zikzakmedia.com>
2011-07-05 13:47:45 +0200

    [ADD] Product Trademark: Add trademark (manafacturer) field and manufacturer information. Also, available on sale, purchase and stock views (and search)

---
+++ product_trademark/product.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2011 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
+#                       Raimon Esteve <resteve@zikzakmedia.com>
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+
+class product_template(osv.osv):
+    _inherit = 'product.template'
+
+    _columns = {
+        'manufacturer': fields.many2one('res.partner', 'Manufacturer'),
+        'manufacturer_pname': fields.char('Name', size=64, help="Manufacturer Product Name"),
+        'manufacturer_pref': fields.char('Code', size=64, help="Manufacturer Product Code"),
+    }
+product_template()

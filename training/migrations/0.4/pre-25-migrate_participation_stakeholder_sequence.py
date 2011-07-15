debeed7e24ac88107604e071bb657781cfc01ea9
Jordi Esteve <jesteve@zikzakmedia.com>
2011-07-15 12:09:14 +0200

    [IMP] Training modules: Move/Remove addons-extra branch to openerp-training branch: https://code.launchpad.net/~openerp-training-community (not duplicate source code projects)

--- training/migrations/0.4/pre-25-migrate_participation_stakeholder_sequence.py
+++
@@ -1,4 +0,0 @@
-__name__ = "Rename the sequence of the participation request"
-
-def migrate(cr, v):
-    cr.execute('ALTER SEQUENCE training_participation_stakeholder_session_id_seq RENAME TO training_participation_stakeholder_request_id_seq')

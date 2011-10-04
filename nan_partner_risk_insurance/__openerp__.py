c4509573c82e597e8c27a4a47002bd0df0ff5fac
Omar (pexego) <omar@pexego.es>
2011-10-04 19:46:02 +0200

    [ADD] nan_partner_risk and nan_partner_risk_insurance ported to 6.0 by Alberto (Pexego)

---
+++ nan_partner_risk_insurance/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+{
+	"name" : "Partner Risk Insurance",
+	"version" : "0.1",
+	"description" : """This module adds a new tab in the partner form to introduce risk insurance information.""",
+	"author" : "NaN for Trod y Avia, S.L.",
+	"website" : "http://www.NaN-tic.com",
+	"depends" : [ 
+		'base', 
+	], 
+	"category" : "Custom Modules",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : [
+		'partner_view.xml',
+	],
+	"active": False,
+	"installable": True
+}

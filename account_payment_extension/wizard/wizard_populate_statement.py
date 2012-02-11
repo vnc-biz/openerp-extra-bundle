f0a6d520e57ba98b07a046f1562cba7008c0beb1
Jordi Esteve <jesteve@zikzakmedia.com>
2012-02-11 13:13:57 +0100

    [FIX] account_payment_extension: The importing of payment lines in bank statements fails in receivable payments lines (after the improvement of showing positive amounts in receivable orders committed several months ago).

--- account_payment_extension/wizard/wizard_populate_statement.py
+++ account_payment_extension/wizard/wizard_populate_statement.py
@@ -70,7 +70,7 @@ def _populate_statement(obj, cr, uid, data, context):
     for line in line_obj.browse(cr, uid, line_ids, context=context):
         ctx = context.copy()
         ctx['date'] = line.ml_maturity_date
-        amount_currency = line.type == 'payment' and line.amount_currency or -line.amount_currency
+        amount_currency = line.type == 'receivable' and line.amount_currency or -line.amount_currency
         amount = currency_obj.compute(cr, uid, line.currency.id,
                 statement.currency.id, amount_currency, context=ctx)
 
@@ -107,8 +107,7 @@ def _populate_statement(obj, cr, uid, data, context):
 
         statement_line_obj.create(cr, uid, {
             'name': (line.order_id.reference or '?') +'. '+ line.name,
-            # Tipically: type=='payable' => amount>0  type=='receivable' => amount<0
-            'amount': line.type == 'payable' and amount or -amount,
+            'amount': amount, 
             'type': line.order_id.type=='payable' and 'supplier' or 'customer',
             'partner_id': line.partner_id.id,
             'account_id': line.move_line_id.account_id.id,

a944a83a58e59b92140259f7a89155784a45430e
Jordi Esteve <jesteve@zikzakmedia.com>
2012-03-02 20:01:29 +0100

    [ADD] stock_extension: New module that adds several improvements to the stock module:       - Add 'To Invoice' filter button to the 'Picking In' search view.       - Remove readonly attribute of the invoice_state field to allow to invoice any stock picking (for example, if someone deletes by mistake the invoice of invoiced picking).       - Fix bug when the Process Picking Wizard is cancelled: Clears the buffer to prevent that previous lines of picking are added to the current wizard.       - When the invoice lines are created from an IN picking, the price unit of the picking is added instead of the cost price of the product.       - Changing the average price of a product: If valuation == 'manual_periodic' is not necessary create an account move for stock variation.

---
+++ stock_extension/stock.py
@@ -0,0 +1,62 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2012 Zikzakmedia S.L. (http://zikzakmedia.com)
+#                       All Rights Reserved.
+#                       Jordi Esteve <jesteve@zikzakmedia.com>
+#                       Jesús Martín <jmartin@zikzakmedia.com>
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+import netsvc
+
+
+class stock_picking(osv.osv):
+    _inherit = "stock.picking"
+
+    # Removed readonly attribute to allow to invoice any stock picking
+    # (for example, if someone deletes by mistake the invoice of invoiced picking)
+    _columns = {
+        'invoice_state': fields.selection([
+            ("invoiced", "Invoiced"),
+            ("2binvoiced", "To Be Invoiced"),
+            ("none", "Not Applicable")], "Invoice Control",
+            select=True, required=True),
+    }
+
+    def _get_price_unit_invoice(self, cr, uid, move_line, type, context=None):
+        """ Gets price unit for invoice
+        IN pickings: Gets the price unit of the picking instead of the cost price of the product
+        @param move_line: Stock move lines
+        @param type: Type of invoice
+        @return: The price unit for the move line
+        """
+        if context is None:
+            context = {}
+
+        if type in ('in_invoice', 'in_refund'):
+            # Take the user company and pricetype
+            context['currency_id'] = move_line.company_id.currency_id.id
+            # Gets the price unit of the picking instead of the cost price of the product
+            amount_unit = move_line.price_unit
+            return amount_unit
+        else:
+            return move_line.product_id.list_price
+
+stock_picking()

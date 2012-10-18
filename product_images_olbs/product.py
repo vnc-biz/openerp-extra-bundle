04c3fce5dc9c0fd41217d97881544338b6039139
Enrico Weigelt, metux ITS <weigelt@metux.de>
2012-10-18 04:42:50 +0200

    product_images_olbs: removed database dependency for image gallery and made it only dependent on file system.

--- product_images_olbs/product.py
+++ product_images_olbs/product.py
@@ -1,5 +1,7 @@
+# -*- encoding: utf-8 -*-
 #########################################################################
 # Copyright (C) 2009  Sharoon Thomas, Open Labs Business solutions      #
+# Copyright (C) 2011 Akretion Sébastien BEAU sebastien.beau@akretion.com#
 #                                                                       #
 #This program is free software: you can redistribute it and/or modify   #
 #it under the terms of the GNU General Public License as published by   #
@@ -15,14 +17,70 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
 #########################################################################
 from osv import osv,fields
+from tools.translate import _
+import os
 
 class product_product(osv.osv):
     _inherit = "product.product"
+
+    def copy(self, cr, uid, id, default=None, context=None):
+        if not default:
+            default = {}
+        default.update({
+            'default_code': False,
+            'images_ids': False,
+        })
+        return super(product_product, self).copy(cr, uid, id, default, context=context)
+
+    def get_main_image(self, cr, uid, id, context=None):
+        if isinstance(id, list):
+            id = id[0]
+        images_ids = self.read(cr, uid, id, ['image_ids'], context=context)['image_ids']
+        if images_ids:
+            return images_ids[0]
+        return False
+
+    def _get_main_image(self, cr, uid, ids, field_name, arg, context=None):
+        res = {}
+        img_obj = self.pool.get('product.images')
+        for id in ids:
+            image_id = self.get_main_image(cr, uid, id, context=context)
+            if image_id:
+                image = img_obj.browse(cr, uid, image_id, context=context)
+                res[id] = image.file
+            else:
+                res[id] = False
+        return res
+
     _columns = {
         'image_ids':fields.one2many(
                 'product.images',
                 'product_id',
                 'Product Images'
-        )
+        ),
+        'default_code' : fields.char('Reference', size=64, require='True'),
+        'product_image': fields.function(_get_main_image, type="binary", method=True),
     }
+
+    def write(self, cr, uid, ids, vals, context=None):
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        # here we expect that the write on default_code is alwayse on 1 product because there is an unique constraint on the default code
+        if vals.get('default_code', False) and ids:
+            local_media_repository = self.pool.get('res.company').get_local_media_repository(cr, uid, context=context)
+            if local_media_repository:
+                old_product = self.read(cr, uid, ids[0], ['default_code', 'image_ids'], context=context)
+                res = super(product_product, self).write(cr, uid, ids, vals, context=context)
+                if old_product['image_ids']:
+                    if old_product['default_code'] != vals['default_code']:
+                        old_path = os.path.join(local_media_repository, old_product['default_code'])
+                        if os.path.isdir(old_path):
+                            os.rename(old_path,  os.path.join(local_media_repository, vals['default_code']))
+                return res
+        return super(product_product, self).write(cr, uid, ids, vals, context=context)
+
+    #This constraint should be by default in openerp 
+    _sql_constraints = [('default_code', 'UNIQUE(default_code)',
+                _('Default code should be uniq'))]
+
 product_product()
--- product_images_olbs/product_images.py
+++ product_images_olbs/product_images.py
@@ -1,5 +1,7 @@
+# -*- encoding: utf-8 -*-
 #########################################################################
 # Copyright (C) 2009  Sharoon Thomas, Open Labs Business solutions      #
+# Copyright (C) 2011 Akretion Sébastien BEAU sebastien.beau@akretion.com#
 #                                                                       #
 #This program is free software: you can redistribute it and/or modify   #
 #it under the terms of the GNU General Public License as published by   #
@@ -16,44 +18,140 @@
 #########################################################################
 from osv import osv, fields
 import base64, urllib
+from tools.translate import _
+import os
+import netsvc
+
+import logging
+
+#TODO find a good solution in order to roll back changed done on file system
+#TODO add the posibility to move from a store system to an other (example : moving existing image on database to file system)
 
 class product_images(osv.osv):
     "Products Image gallery"
     _name = "product.images"
     _description = __doc__
     _table = "product_images"
+    _logger = logging.getLogger(_name)
     
-    def get_image(self, cr, uid, id):
-        each = self.read(cr, uid, id, ['link', 'filename', 'image'])
+    def unlink(self, cr, uid, ids, context=None):
+        if not isinstance(ids, list):
+            ids=[ids]
+        local_media_repository = self.pool.get('res.company').get_local_media_repository(cr, uid, context=context)
+        if local_media_repository:
+            for image in self.browse(cr, uid, ids, context=context):
+                path = os.path.join(local_media_repository, image.product_id.default_code, image.name)
+                if os.path.isfile(path):
+                    os.remove(path)          
+        return super(product_images, self).unlink(cr, uid, ids, context=context)
+
+    def create(self, cr, uid, vals, context=None):
+        if vals.get('name', False) and not vals.get('extention', False):
+            vals['name'], vals['extention'] = os.path.splitext(vals['name'])
+        return super(product_images, self).create(cr, uid, vals, context=context)
+
+    def write(self, cr, uid, ids, vals, context=None):
+        if not isinstance(ids, list):
+            ids=[ids]
+        if vals.get('name', False) and not vals.get('extention', False):
+            vals['name'], vals['extention'] = os.path.splitext(vals['name'])
+        if vals.get('name', False) or vals.get('extention', False):
+            local_media_repository = self.pool.get('res.company').get_local_media_repository(cr, uid, context=context)
+            if local_media_repository:
+                old_images = self.browse(cr, uid, ids, context=context)
+                res=[]
+                for old_image in old_images:
+                    if vals.get('name', False) and (old_image.name != vals['name']) or vals.get('extention', False) and (old_image.extention != vals['extention']):
+                        old_path = os.path.join(local_media_repository, old_image.product_id.default_code, '%s%s' %(old_image.name, old_image.extention))
+                        res.append(super(product_images, self).write(cr, uid, old_image.id, vals, context=context))
+                        if 'file' in vals:
+                            #a new image have been loaded we should remove the old image
+                            #TODO it's look like there is something wrong with function field in openerp indeed the preview is always added in the write :(
+                            if os.path.isfile(old_path):
+                                os.remove(old_path)
+                        else:
+                            #we have to rename the image on the file system
+                            if os.path.isfile(old_path):
+                                os.rename(old_path, os.path.join(local_media_repository, old_image.product_id.default_code, '%s%s' %(old_image.name, old_image.extention)))      
+                return res
+        return super(product_images, self).write(cr, uid, ids, vals, context=context)
+
+    def get_image(self, cr, uid, id, context=None):
+        each = self.read(cr, uid, id, ['link', 'url', 'name', 'file_db_store', 'product_id', 'name', 'extention'])
         if each['link']:
-            try:
-                (filename, header) = urllib.urlretrieve(each['filename'])
-                f = open(filename , 'rb')
-                img = base64.encodestring(f.read())
-                f.close()
-            except:
-                img = ''
+            (filename, header) = urllib.urlretrieve(each['url'])
+            f = open(filename , 'rb')
+            img = base64.encodestring(f.read())
+            f.close()
         else:
-            img = each['image']
+            local_media_repository = self.pool.get('res.company').get_local_media_repository(cr, uid, context=context)
+            if local_media_repository:
+                product_code = self.pool.get('product.product').read(cr, uid, each['product_id'][0], ['default_code'])['default_code']
+                full_path = os.path.join(local_media_repository, product_code, '%s%s'%(each['name'], each['extention']))
+                if os.path.exists(full_path):
+                    try:
+                        f = open(full_path, 'rb')
+                        img = base64.encodestring(f.read())
+                        f.close()
+                    except Exception, e:
+                        self._logger.error("Can not open the image %s, error : %s" %(full_path, e))
+                        return False
+                else:
+                    self._logger.error("The image %s doesn't exist " %full_path)
+                    return False
+            else:
+                img = each['file_db_store']
         return img
     
-    def _get_image(self, cr, uid, ids, field_name, arg, context={}):
+    def _get_image(self, cr, uid, ids, field_name, arg, context=None):
         res = {}
         for each in ids:
-            res[each] = self.get_image(cr, uid, each)
+            res[each] = self.get_image(cr, uid, each, context=context)
         return res
-    
+
+    def _check_filestore(self, image_filestore):
+        '''check if the filestore is created, if not it create it automatically'''
+        try:
+            if not os.path.isdir(image_filestore):
+                os.makedirs(image_filestore)
+        except Exception, e:
+            raise osv.except_osv(_('Error'), _('The image filestore can not be created, %s'%e))
+        return True
+
+    def _save_file(self, path, filename, b64_file):
+        """Save a file encoded in base 64"""
+        full_path = os.path.join(path, filename)
+        self._check_filestore(path)
+        ofile = open(full_path, 'w')
+        try:
+            ofile.write(base64.decodestring(b64_file))
+        finally:
+            ofile.close()
+        return True
+
+    def _set_image(self, cr, uid, id, name, value, arg, context=None):
+        local_media_repository = self.pool.get('res.company').get_local_media_repository(cr, uid, context=context)
+        if local_media_repository:
+            image = self.browse(cr, uid, id, context=context)
+            return self._save_file(os.path.join(local_media_repository, image.product_id.default_code), '%s%s'%(image.name, image.extention), value)
+        return self.write(cr, uid, id, {'file_db_store' : value}, context=context)
+
     _columns = {
         'name':fields.char('Image Title', size=100, required=True),
+        'extention': fields.char('file extention', size=6),
         'link':fields.boolean('Link?', help="Images can be linked from files on your file system or remote (Preferred)"),
-        'image':fields.binary('Image', filters='*.png,*.jpg,*.gif'),
-        'filename':fields.char('File Location', size=250),
-        'preview':fields.function(_get_image, type="binary", method=True),
+        'file_db_store':fields.binary('Image stored in database'),
+        'file':fields.function(_get_image, fnct_inv=_set_image, type="binary", filters='*.png,*.jpg,*.gif'),
+        'url':fields.char('File Location', size=250),
         'comments':fields.text('Comments'),
         'product_id':fields.many2one('product.product', 'Product')
     }
 
     _defaults = {
-        'link': lambda *a: True,
+        'link': lambda *a: False,
     }
+
+    _sql_constraints = [('uniq_name_product_id', 'UNIQUE(product_id, name)',
+                _('A product can have only one image with the same name'))]
+
 product_images()
